302a19d761f95a02fb6c65a8783ec1554977bd0d
Jessica Kulwik <jessica@code.org>
2021-02-18 20:01:44 +0000

    Updated javadoc

--- org-code-javabuilder/app/src/main/java/org/code/javabuilder/Destinations.java
+++ org-code-javabuilder/app/src/main/java/org/code/javabuilder/Destinations.java
@@ -1,5 +1,8 @@
 package org.code.javabuilder;
 
+/**
+ * String constants representing websocket channels and destinations.
+ */
 public final class Destinations {
   private Destinations(){}
 
@@ -20,12 +23,12 @@ public final class Destinations {
   public static final String APP_ENDPOINT = "/codebuilder";
 
   /**
-   *
+   * The channel where the client should listen to for output.
    */
   public static final String OUTPUT_CHANNEL = "/output";
 
   /**
-   *
+   * The channel where the client should send code to be compiled/run.
    */
   public static final String EXECUTE_CODE = "/execute";
 }
