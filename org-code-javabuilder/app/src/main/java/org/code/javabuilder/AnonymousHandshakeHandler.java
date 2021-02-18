2c4957bd86fbe7036b323bf7a790e56874d627b9
Jessica Kulwik <jessica@code.org>
2021-02-18 23:44:31 +0000

    Added google java linter and commit hooks

--- org-code-javabuilder/app/src/main/java/org/code/javabuilder/AnonymousHandshakeHandler.java
+++ org-code-javabuilder/app/src/main/java/org/code/javabuilder/AnonymousHandshakeHandler.java
@@ -8,22 +8,18 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 /**
- * Assignes a unique id to each handshake to allow messages to be directed
- * to specific anonymous users.
+ * Assignes a unique id to each handshake to allow messages to be directed to specific anonymous
+ * users.
  */
 public class AnonymousHandshakeHandler extends DefaultHandshakeHandler {
 
   /**
-   * Set anonymous user (Principal) in WebSocket messages by using UUID. This is
-   * necessary to avoid broadcasting messages to all users and instead send them
-   * to specific user sessions.
+   * Set anonymous user (Principal) in WebSocket messages by using UUID. This is necessary to avoid
+   * broadcasting messages to all users and instead send them to specific user sessions.
    */
   @Override
   protected Principal determineUser(
-    ServerHttpRequest request,
-    WebSocketHandler wsHandler,
-    Map<String, Object> attributes
-  ) {
+      ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
     // Assign UUID to a user (Principal)
     return new AnonymousUser(UUID.randomUUID().toString());
   }
