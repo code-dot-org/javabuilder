aeaf419f4b5eb25abd28ec4705eb8d5f00edae8f
Jessica Kulwik <jessica@code.org>
2021-02-18 23:44:31 +0000

    Added google java linter and commit hooks

--- org-code-javabuilder/app/src/main/java/org/code/javabuilder/WebSocketConfig.java
+++ org-code-javabuilder/app/src/main/java/org/code/javabuilder/WebSocketConfig.java
@@ -6,17 +6,14 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-/**
- * Configures websocket channels to send messages to specific users
- */
+/** Configures websocket channels to send messages to specific users */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   /**
-   * Sets up the channels that a client can subscribe to. Specifically, a client
-   * would subscribe to /topic/destination and would send messages to
-   * /app/destination.
+   * Sets up the channels that a client can subscribe to. Specifically, a client would subscribe to
+   * /topic/destination and would send messages to /app/destination.
    */
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -25,19 +22,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   }
 
   /**
-   * Sets up a custom endpoint where users will initially connect. Overrides
-   * the HandshakeHandler in order to return messages to specific users rather
-   * than the entire channel.
+   * Sets up a custom endpoint where users will initially connect. Overrides the HandshakeHandler in
+   * order to return messages to specific users rather than the entire channel.
    */
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry
-      .addEndpoint(Destinations.APP_ENDPOINT)
-      .setHandshakeHandler(new AnonymousHandshakeHandler());
+        .addEndpoint(Destinations.APP_ENDPOINT)
+        .setHandshakeHandler(new AnonymousHandshakeHandler());
 
     registry
-      .addEndpoint(Destinations.APP_ENDPOINT)
-      .setHandshakeHandler(new AnonymousHandshakeHandler())
-      .withSockJS();
+        .addEndpoint(Destinations.APP_ENDPOINT)
+        .setHandshakeHandler(new AnonymousHandshakeHandler())
+        .withSockJS();
   }
 }
