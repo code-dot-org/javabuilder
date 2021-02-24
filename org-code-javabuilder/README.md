e6ed8c9f7885d06d22cbf76fbed15347e140cd14
molly-moen <molly@code.org>
2021-02-24 14:34:05 -0800

    initial run functionality

---
+++ org-code-javabuilder/README.md
@@ -0,0 +1,8 @@
+# JavaBuilder App
+
+## Testing
+To test, run the following commands from the `org-code-javabuilder` folder.
+```
+gradle bootRun
+open localhost:8080
+```
--- org-code-javabuilder/app/src/main/java/org/code/javabuilder/JavaBuildController.java
+++ org-code-javabuilder/app/src/main/java/org/code/javabuilder/JavaBuildController.java
@@ -1,13 +1,22 @@
 package org.code.javabuilder;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.security.Principal;
-import org.apache.commons.io.FilenameUtils;
+import java.util.Arrays;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
@@ -30,68 +39,72 @@ public class JavaBuildController {
   @SendToUser(Destinations.PTP_PREFIX + Destinations.OUTPUT_CHANNEL)
   public UserProgramOutput execute(UserProgram userProgram, Principal principal) throws Exception {
     // TODO: CSA-48 Handle more than one file
-    // if (userProject.size() > 1) {
-    //   return "Error: Can only process one file at a time.";
-    // }
+    executeHelper(userProgram, principal);
+    return new UserProgramOutput("Ran program!");
+  }
 
-    File compileRunScript = null;
-    String tempDir = System.getProperty("java.io.tmpdir");
-
-    String filename = userProgram.getFileName();
-    String className = FilenameUtils.removeExtension(filename);
-    String userCode = userProgram.getCode();
+  public void executeHelper(UserProgram userProgram, Principal principal) {
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+    File tempFolder = null;
     try {
-      // Build user's java file
-      File userFile = new File(Paths.get(tempDir, filename).toString());
-      FileWriter writer = new FileWriter(userFile);
-      writer.write(userCode);
-      writer.close();
+      tempFolder = Files.createTempDirectory("tmpdir").toFile();
+      System.out.println(tempFolder.getAbsolutePath());
+      fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(tempFolder));
 
-      // Create the compile & run script
-      compileRunScript = File.createTempFile("script", null);
-      FileWriter scriptWriter = new FileWriter(compileRunScript);
-      scriptWriter.write("javac " + userFile.getAbsolutePath());
-      scriptWriter.write(System.getProperty("line.separator"));
-      scriptWriter.write("java -cp " + tempDir + " " + className);
-      scriptWriter.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-      // TODO: CSA-42 Improve error handling
-      return new UserProgramOutput("An error occurred creating files.");
-    }
+      DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+      JavaFileObject file =
+          new JavaSourceFromString(userProgram.getFileName(), userProgram.getCode());
+      Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
+      CompilationTask task =
+          compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+      boolean success = task.call();
 
-    ProcessBuilder pb = new ProcessBuilder().command("bash", compileRunScript.toString());
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      PrintStream out = new PrintStream(outputStream);
+      System.setOut(out);
 
-    Process process = null;
-    try {
-      // Execute the user's code
-      // TODO: CSA-42 Handle infinite loops, malicious code, etc.
-      process = pb.start();
-      process.waitFor();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-      return new UserProgramOutput("An error occurred running the program.");
-    } catch (IOException e) {
-      e.printStackTrace();
-      return new UserProgramOutput("An error occurred running the program.");
-    }
-
-    StringBuilder programOutput = new StringBuilder();
-    try {
-      // Get output from the user's program
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String line = null;
-      while ((line = reader.readLine()) != null) {
-        programOutput.append(line);
+      if (success) {
+        runClass(tempFolder.toURI().toURL(), userProgram.getFileName());
       }
+      outputStream.flush();
+      String result = outputStream.toString();
+      compileRunService.sendMessages(principal.getName(), result);
     } catch (IOException e) {
-      e.printStackTrace();
-      return new UserProgramOutput("An error occurred reading the program output.");
+      System.out.println(e.getStackTrace());
     }
+    if (tempFolder != null) {
+      tempFolder.delete();
+    }
+  }
 
-    // return new UserProgramOutput("> " + programOutput.toString());
-    compileRunService.sendMessages(principal.getName(), programOutput.toString());
-    // Haha this line gets sent second.
-    return new UserProgramOutput("> Compiling and running your code...");
+  public void runClass(URL filePath, String className) {
+    URL[] classLoaderUrls = new URL[] {filePath};
+
+    // Create a new URLClassLoader
+    URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
+
+    // Load the target class
+    try {
+      Class<?> hello = urlClassLoader.loadClass(className);
+      hello
+          .getDeclaredMethod("main", new Class[] {String[].class})
+          .invoke(null, new Object[] {null});
+
+    } catch (ClassNotFoundException e) {
+      System.err.println("Class not found: " + e);
+    } catch (NoSuchMethodException e) {
+      System.err.println("No such method: " + e);
+    } catch (IllegalAccessException e) {
+      System.err.println("Illegal access: " + e);
+    } catch (InvocationTargetException e) {
+      System.err.println("Invocation target: " + e);
+    }
+    try {
+      urlClassLoader.close();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
