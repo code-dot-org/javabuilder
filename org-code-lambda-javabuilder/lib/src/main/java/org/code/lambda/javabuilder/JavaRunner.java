ff3aaf0ef5f69de0cbe1572959cac0bde76f46e2
Jessica Kulwik <jessica@code.org>
2021-03-25 20:29:22 -0700

    Threading issues fixed

--- org-code-lambda-javabuilder/lib/src/main/java/org/code/lambda/javabuilder/JavaRunner.java
+++ org-code-lambda-javabuilder/lib/src/main/java/org/code/lambda/javabuilder/JavaRunner.java
@@ -6,9 +6,15 @@ public class JavaRunner extends Thread {
   public JavaRunner() {}
 
   public void run() {
-    System.out.println("What's your name? ");
-    Scanner in = new Scanner(System.in);
-    String s = in.nextLine();
-    System.out.println("Hello " + s + "!");
+    System.out.println("Hello world!");
+//    System.out.println("What's your name? ");
+//    Scanner in = new Scanner(System.in);
+//    String s = in.nextLine();
+//    System.out.println("Hello " + s + "!");
+//    System.out.println("How");
+//    System.out.println("are");
+//    System.out.println("you?");
+    OutputHandler.sendDebuggingMessage("Done running code");
+    OutputSemaphore.signalProcessFinalOutput();
   }
 }
