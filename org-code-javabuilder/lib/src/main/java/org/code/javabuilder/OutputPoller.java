e00b8f12df89c26c86e09a4e6401ad30696c59c3
Jessica Kulwik <jessica@code.org>
2021-03-26 16:53:44 -0700

    Migrated to the org-code-javabuilder folder

---
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/OutputPoller.java
@@ -0,0 +1,39 @@
+package org.code.javabuilder;
+
+import java.io.IOException;
+
+public class OutputPoller extends Thread {
+  private final JavaRunner javaRunner;
+  private final OutputHandler outputHandler;
+  private final RuntimeIO runtimeIO;
+  private final OutputSemaphore outputSemaphore;
+  public OutputPoller(JavaRunner javaRunner, OutputHandler outputHandler, RuntimeIO runtimeIO, OutputSemaphore outputSemaphore) {
+    this.javaRunner = javaRunner;
+    this.outputHandler = outputHandler;
+    this.runtimeIO = runtimeIO;
+    this.outputSemaphore = outputSemaphore;
+  }
+
+  public void run() {
+    while (javaRunner.isAlive() || outputSemaphore.anyOutputInProgress()) {
+      if(!javaRunner.isAlive()) {
+        outputSemaphore.processFinalOutput();
+      }
+      String message = null;
+      try {
+        message = runtimeIO.pollForOutput();
+      } catch (IOException e) {
+        outputHandler.sendMessage("There was an error reading output from your program. Try running it again." + e.toString());
+      }
+      if (message != null) {
+        outputHandler.sendMessage(message);
+      }
+
+      try {
+        Thread.sleep(400);
+      } catch (InterruptedException e) {
+        outputHandler.sendMessage("There was an error reading output from your program. Try running it again." + e.toString());
+      }
+    }
+  }
+}
---
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/RuntimeIO.java
@@ -0,0 +1,64 @@
+package org.code.javabuilder;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class RuntimeIO {
+  private final PrintStream stdout;
+  private final InputStream stdin;
+  private final PipedOutputStream systemInputWriter;
+  private final BufferedReader systemOutputReader;
+  private final PipedOutputStream systemOutputStream;
+  private final OutputSemaphore outputSemaphore;
+
+  public RuntimeIO(OutputSemaphore outputSemaphore) throws IOException {
+    this.outputSemaphore = outputSemaphore;
+
+    // -- Redirect output from the user's program --
+    PipedInputStream inputStream = new PipedInputStream();
+
+    // Where can we read output from the user's program
+    this.systemOutputReader = new BufferedReader(new InputStreamReader(inputStream));
+
+    // The stream that collects output from the user's program. Flush this to read new output.
+    this.systemOutputStream = new PipedOutputStream(inputStream);
+
+    // Overwrite System.out
+    this.stdout = System.out;
+    System.setOut(new PrintStream(systemOutputStream));
+
+    // -- Redirect input to the user's program --
+    PipedInputStream systemInputStream = new PipedInputStream();
+
+    // Where we can write input to the user's program
+    this.systemInputWriter = new PipedOutputStream(systemInputStream);
+
+    // Overwrite System.in
+    this.stdin = System.in;
+    System.setIn(systemInputStream);
+  }
+
+  public void passInputToProgram(String userInput) throws IOException {
+    // System.in expects input to end with a lineSeparator. We add that to allow the user program to
+    // continue.
+    byte[] input = (userInput + System.lineSeparator()).getBytes(StandardCharsets.UTF_8);
+    systemInputWriter.write(input, 0, input.length);
+    systemInputWriter.flush();
+  }
+
+  public String pollForOutput() throws IOException {
+    systemOutputStream.flush();
+    if(systemOutputReader.ready()) {
+      outputSemaphore.addOutputInProgress();
+      return systemOutputReader.readLine();
+    } else {
+      outputSemaphore.decreaseOutputInProgress();
+      return null;
+    }
+  }
+
+  public void restoreSystemIO() {
+    System.setOut(stdout);
+    System.setIn(stdin);
+  }
+}
