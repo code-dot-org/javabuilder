bce015d10e226811347a064fe1f3152165e7de75
Jessica Kulwik <jessica@code.org>
2021-03-29 17:53:08 -0700

    Removed old version of output redirector

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavaRunner.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavaRunner.java
@@ -3,19 +3,14 @@ package org.code.javabuilder;
 import java.util.Scanner;
 
 public class JavaRunner extends Thread {
-  private final OutputSemaphore outputSemaphore;
-  public JavaRunner(OutputSemaphore outputSemaphore) {
-    this.outputSemaphore = outputSemaphore;
-  }
-
   public void run() {
     // Sample program. In the future, student code will be executed here.
     System.out.println("What's your name? ");
     Scanner in = new Scanner(System.in);
     String s = in.nextLine();
     System.out.println("Hello " + s + "!");
-
-    // Tell the output poller to collect any remaining output from the program.
-//    outputSemaphore.signalProcessFinalOutput();
+    System.out.println("How");
+    System.out.println("are");
+    System.out.print("you?");
   }
 }
--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/RuntimeIO.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/RuntimeIO.java
@@ -7,57 +7,11 @@ public class RuntimeIO {
   private final PrintStream stdout;
   private final InputStream stdin;
   private final PipedOutputStream systemInputWriter;
-  private BufferedReader systemOutputReader;
-  private PipedOutputStream systemOutputStream;
-  private OutputSemaphore outputSemaphore;
-
-  public RuntimeIO(OutputSemaphore outputSemaphore) throws IOException {
-    this.outputSemaphore = outputSemaphore;
-
-    // -- Redirect output from the user's program --
-    PipedInputStream inputStream = new PipedInputStream();
-
-    // Where can we read output from the user's program
-    this.systemOutputReader = new BufferedReader(new InputStreamReader(inputStream));
-
-    // The stream that collects output from the user's program. Flush this to read new output.
-    this.systemOutputStream = new PipedOutputStream(inputStream);
-
-    // Overwrite System.out
-    this.stdout = System.out;
-    System.setOut(new PrintStream(systemOutputStream));
-
-    // -- Redirect input to the user's program --
-    PipedInputStream systemInputStream = new PipedInputStream();
-
-    // Where we can write input to the user's program
-    this.systemInputWriter = new PipedOutputStream(systemInputStream);
-
-    // Overwrite System.in
-    this.stdin = System.in;
-    System.setIn(systemInputStream);
-  }
 
   public RuntimeIO(OutputStream outputStream) throws IOException {
     // Overwrite System.out
     this.stdout = System.out;
     System.setOut(new PrintStream(outputStream, true));
-
-    // -- Redirect input to the user's program --
-    PipedInputStream systemInputStream = new PipedInputStream();
-
-    // Where we can write input to the user's program
-    this.systemInputWriter = new PipedOutputStream(systemInputStream);
-
-    // Overwrite System.in
-    this.stdin = System.in;
-    System.setIn(systemInputStream);
-  }
-
-  public RuntimeIO(CustomOutputStream customOutputStream) throws IOException {
-    // Overwrite System.out
-    this.stdout = System.out;
-    System.setOut(customOutputStream);
 
     // -- Redirect input to the user's program --
     PipedInputStream systemInputStream = new PipedInputStream();
@@ -76,17 +30,6 @@ public class RuntimeIO {
     byte[] input = (userInput + System.lineSeparator()).getBytes(StandardCharsets.UTF_8);
     systemInputWriter.write(input, 0, input.length);
     systemInputWriter.flush();
-  }
-
-  public String pollForOutput() throws IOException {
-    systemOutputStream.flush();
-    if(systemOutputReader.ready()) {
-      outputSemaphore.addOutputInProgress();
-      return systemOutputReader.readLine();
-    } else {
-      outputSemaphore.decreaseOutputInProgress();
-      return null;
-    }
   }
 
   public void restoreSystemIO() {
