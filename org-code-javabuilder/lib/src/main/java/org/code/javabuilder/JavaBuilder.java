56a548a524e1a3e4574375345668dbe81dd1800b
Jessica Kulwik <jessica@code.org>
2021-03-29 17:53:08 -0700

    Removed old version of output redirector

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavaBuilder.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavaBuilder.java
@@ -6,51 +6,34 @@ public class JavaBuilder {
   private final OutputAdapter outputAdapter;
   private final InputAdapter inputAdapter;
   private final InputPoller inputPoller;
-//  private final OutputPoller outputPoller;
   private final JavaRunner javaRunner;
-  private final OutputSemaphore outputSemaphore;
 
   public JavaBuilder(InputAdapter inputAdapter, OutputAdapter outputAdapter) {
     this.outputAdapter = outputAdapter;
     this.inputAdapter = inputAdapter;
-    this.outputSemaphore = new OutputSemaphore();
 
     // Overwrite system I/O
     RuntimeIO runtimeIO;
-//    PrintStream printStream = new PrintStream();
-    CustomOutputStream customOutputStream = new CustomOutputStream(System.out, this.outputAdapter);
     OutputRedirectionStream outputRedirectionStream = new OutputRedirectionStream(this.outputAdapter);
 
     try {
-      runtimeIO = new RuntimeIO(outputRedirectionStream);//this.outputSemaphore);
+      runtimeIO = new RuntimeIO(outputRedirectionStream);
     } catch (IOException e) {
       this.outputAdapter.sendMessage("There was an error running your code. Try again.");
       throw new RuntimeException("Error setting up console IO", e);
     }
 
     // Create code runner
-    this.javaRunner = new JavaRunner(this.outputSemaphore);
+    this.javaRunner = new JavaRunner();
 
     // Create input poller
     this.inputPoller = new InputPoller(this.inputAdapter, runtimeIO, this.javaRunner, this.outputAdapter);
-
-    // Create output poller
-//    this.outputPoller = new OutputPoller(this.javaRunner, this.outputAdapter, runtimeIO, this.outputSemaphore);
   }
 
   public void runUserCode() {
     this.javaRunner.start();
     this.inputPoller.start();
-//    this.outputPoller.start();
     while(javaRunner.isAlive()) {
-      try {
-        Thread.sleep(400);
-      } catch (InterruptedException e) {
-        outputAdapter.sendMessage("There was an error running to your program. Try running it again." + e.toString());
-      }
-    }
-
-    while (outputSemaphore.anyOutputInProgress()) {
       try {
         Thread.sleep(400);
       } catch (InterruptedException e) {
