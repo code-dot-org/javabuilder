c11b3cc4463f92c54c32476839626045bb1ec978
Jessica Kulwik <jessica@code.org>
2021-04-02 12:27:49 -0700

    In progress

---
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/ProjectFile.java
@@ -0,0 +1,41 @@
+package org.code.javabuilder;
+
+public class ProjectFile {
+  private String fileName;
+  private String code;
+  private String className;
+
+  public ProjectFile(String fileName, String code) throws Exception {
+    this.fileName = fileName;
+    this.code = code;
+    if (fileName.indexOf(".java") > 0) {
+      this.className = fileName.substring(0, fileName.indexOf(".java"));
+    } else {
+      throw new Exception("Invalid File Name. File name must end in '.java'.");
+    }
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public String getClassName() {
+    return this.className;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public void setClassName(String className) {
+    this.className = className;
+  }
+}
