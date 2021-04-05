e6653b0e3a81439d9e21f2480951070c5af9b080
Jessica Kulwik <jessica@code.org>
2021-04-05 16:48:40 -0700

    Removed files that will be in a later code review.

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/UserProjectFileManager.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/UserProjectFileManager.java
@@ -1,44 +1,15 @@
 package org.code.javabuilder;
 
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class UserProjectFileManager implements ProjectFileManager {
-  private final String baseUrl;
-  private final String[] fileNames;
-  private final ArrayList<ProjectFile> fileList;
 
-  public UserProjectFileManager(String baseUrl, String[] fileNames) {
-    this.baseUrl = baseUrl;
-    this.fileNames = fileNames;
-    this.fileList = new ArrayList<>();
-  }
+  public UserProjectFileManager(String baseUrl, String[] fileNames) {}
 
   public void loadFiles() throws UserFacingException {
-    ExecutorService executor = Executors.newCachedThreadPool();
-    for (String fileName : fileNames) {
-      ProjectFile file = new ProjectFile(fileName);
-      fileList.add(file);
-      executor.execute(new ProjectLoader(file, String.join(baseUrl, fileName, "/")));
-    }
-    executor.shutdown();
-    boolean finished;
-    try {
-      finished = executor.awaitTermination(10, TimeUnit.SECONDS);
-    } catch (InterruptedException e) {
-      throw new UserFacingException(
-          "We hit an error on our side while loading your files. Try again.");
-    }
-    if (!finished) {
-      throw new UserFacingException(
-          "We couldn't fetch your files before our loader timed out. Try again.");
-    }
+    throw new UserFacingException("We couldn't load your files");
   }
 
   @Override
   public ProjectFile getFile() {
-    return fileList.get(0);
+    return null;
   }
 }
