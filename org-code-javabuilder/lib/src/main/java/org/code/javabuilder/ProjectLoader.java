37aa1698dfdfd7bf75317e0c2abfb6662b7faf54
Jessica Kulwik <jessica@code.org>
2021-04-05 17:22:21 -0700

    Revert "Removed files that will be in a later code review."
    This reverts commit 0c34b752101c1094e5a61040db50b4ecf895e8dc.

---
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/ProjectLoader.java
@@ -0,0 +1,56 @@
+package org.code.javabuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ProjectLoader extends Thread {
+  private final ProjectFile projectFile;
+  private final String fileUrl;
+
+  public ProjectLoader(ProjectFile projectFile, String fileUrl) {
+    this.projectFile = projectFile;
+    this.fileUrl = fileUrl;
+  }
+
+  public void run() {
+    URL url = null;
+    try {
+      url = new URL(fileUrl);
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+    try {
+      assert url != null;
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      try {
+        con.setRequestMethod("GET");
+      } catch (ProtocolException e) {
+        e.printStackTrace();
+      }
+      int status = con.getResponseCode();
+      Reader streamReader = null;
+      if (status > 299) {
+        streamReader = new InputStreamReader(con.getErrorStream());
+      } else {
+        streamReader = new InputStreamReader(con.getInputStream());
+      }
+      BufferedReader in = new BufferedReader(streamReader);
+      String inputLine;
+      StringBuffer content = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+        content.append(inputLine);
+      }
+      in.close();
+      con.disconnect();
+      projectFile.setCode(content.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
