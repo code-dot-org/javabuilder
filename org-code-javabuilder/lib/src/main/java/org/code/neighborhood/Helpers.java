73c1968f54799cf62e8d39e48f0657fa87f8ded5
JillianK <jillian9012@gmail.com>
2021-05-10 16:39:44 -0700

    fix constant naming

--- org-code-javabuilder/lib/src/main/java/org/code/neighborhood/Helpers.java
+++ org-code-javabuilder/lib/src/main/java/org/code/neighborhood/Helpers.java
@@ -5,12 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Helpers {
-    private static final String HEX_WEBCOLOR_PATTERN
+    private static final String HEX_WEB_COLOR_PATTERN
             = "^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$";
-    private static final Pattern pattern = Pattern.compile(HEX_WEBCOLOR_PATTERN);
-    private static final Set<String> colors = Set.of("white", "silver", "gray", "black", "red", "maroon", "yellow", "olive", "lime", "green", "aqua", "teal", "blue", "navy", "fuchsia", "purple");
+    private static final Pattern PATTERN = Pattern.compile(HEX_WEB_COLOR_PATTERN);
+    private static final Set<String> WEB_COLORS = Set.of("white", "silver", "gray", "black", "red", "maroon", "yellow", "olive", "lime", "green", "aqua", "teal", "blue", "navy", "fuchsia", "purple");
     public static boolean isColor(String color) {
-        Matcher matcher = pattern.matcher(color);
-        return matcher.matches() || colors.contains(color);
+        Matcher matcher = PATTERN.matcher(color);
+        return matcher.matches() || WEB_COLORS.contains(color);
     }
 }
