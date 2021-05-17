a7f26cf7b6d701ebeb4dd3fb8ba6c5e37ecb39a4
Jessica Kulwik <jessica@code.org>
2021-05-17 12:59:05 -0700

    fixed up some comment strings

--- org-code-javabuilder/neighborhood/src/main/java/org/code/neighborhood/NeighborhoodOutputHandler.java
+++ org-code-javabuilder/neighborhood/src/main/java/org/code/neighborhood/NeighborhoodOutputHandler.java
@@ -2,7 +2,8 @@ package org.code.neighborhood;
 
 public class NeighborhoodOutputHandler {
   public static void sendMessage(NeighborhoodSignalMessage message) {
-    // This is a hack that we are temporarily using while we design a better system to handle passing signals from Javabuilder mini apps to Java Lab
+    // This is a hack that we are temporarily using while we design a better
+    // system to handle passing signals from Javabuilder mini apps to Java Lab
     // TODO: replace this with a more formal messaging system.
     System.out.print(message.getFormattedMessage());
   }
