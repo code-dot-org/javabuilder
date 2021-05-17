14dba2353cd11f26acfceacbb0fb23729e7d3ed2
Jessica Kulwik <jessica@code.org>
2021-05-17 08:55:18 -0700

    Fixed broken test

--- org-code-javabuilder/lib/src/test/java/org/code/javabuilder/SystemOutMessageTest.java
+++ org-code-javabuilder/lib/src/test/java/org/code/javabuilder/SystemOutMessageTest.java
@@ -2,6 +2,7 @@ package org.code.javabuilder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.code.util.ClientMessage;
 import org.junit.jupiter.api.Test;
 
 public class SystemOutMessageTest {
