d97c8d35335e4628e034c6613b6a8d032b8be0f2
molly-moen <molly@code.org>
2021-05-18 16:32:06 -0700

    add more neighborhood signals

--- org-code-javabuilder/neighborhood/src/main/java/org/code/neighborhood/NeighborhoodSignalKey.java
+++ org-code-javabuilder/neighborhood/src/main/java/org/code/neighborhood/NeighborhoodSignalKey.java
@@ -1,6 +1,18 @@
 package org.code.neighborhood;
 
 public enum NeighborhoodSignalKey {
+  // Initialize a new painter
+  INITIALIZE,
   // Move the painter one space forward
-  MOVE
+  MOVE,
+  // Paint the current location
+  PAINT,
+  // Remove all paint from current location
+  REMOVE_PAINT,
+  // Take paint from the bucket
+  TAKE_PAINT,
+  // Hide the painter on the screen
+  HIDE_PAINTER,
+  // Show the painter on the screen
+  SHOW_PAINTER
 }
