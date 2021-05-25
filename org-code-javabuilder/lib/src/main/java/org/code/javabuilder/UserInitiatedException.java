2ff4081748d1f208046fb1851a542c4121cd122f
Jessica Kulwik <jessica@code.org>
2021-05-24 17:14:35 -0700

    Set common parent for all user-facing exceptions

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/UserInitiatedException.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/UserInitiatedException.java
@@ -1,38 +1,14 @@
 package org.code.javabuilder;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.HashMap;
+import org.code.protocol.JavabuilderException;
 
 /** An exception caused by a user action. */
-public class UserInitiatedException extends Exception {
-  private final UserInitiatedExceptionKey key;
-
+public class UserInitiatedException extends JavabuilderException {
   public UserInitiatedException(UserInitiatedExceptionKey key) {
-    super(key.toString());
-    this.key = key;
+    super(key);
   }
 
   public UserInitiatedException(UserInitiatedExceptionKey key, Throwable cause) {
-    super(key.toString(), cause);
-    this.key = key;
-  }
-
-  // TODO: Correctly print error messages.
-  public UserInitiatedExceptionMessage getExceptionMessage() {
-    HashMap<String, String> detail = new HashMap<>();
-    if (this.getCause() != null) {
-      detail.put("cause", this.getLoggingString());
-    }
-
-    return new UserInitiatedExceptionMessage(this.key, detail.size() > 0 ? detail : null);
-  }
-
-  /** @return A pretty version of the exception and stack trace. */
-  public String getLoggingString() {
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter printWriter = new PrintWriter(stringWriter);
-    this.printStackTrace(printWriter);
-    return stringWriter.toString();
+    super(key, cause);
   }
 }
