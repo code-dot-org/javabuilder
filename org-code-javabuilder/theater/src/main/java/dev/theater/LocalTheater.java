ab0f170bb95a9b609a50ee77e04cbb4f68699a5e
molly-moen <molly@code.org>
2021-06-11 12:22:59 -0700

    add comments

--- org-code-javabuilder/theater/src/main/java/dev/theater/LocalTheater.java
+++ org-code-javabuilder/theater/src/main/java/dev/theater/LocalTheater.java
@@ -1,11 +1,10 @@
 package dev.theater;
 
-import java.io.IOException;
 import org.code.theater.CatImage;
 
 /** Intended for local testing only. */
 public class LocalTheater {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     CatImage image = new CatImage();
     image.buildImageFilter(0);
     image.buildCanvas(1000);
