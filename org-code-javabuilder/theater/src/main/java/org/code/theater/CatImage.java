f6576b7d45cee9279980048b71b2a97c9ded3d21
Jessica Kulwik <jessica@code.org>
2021-06-18 15:07:23 -0700

    Initial test

--- org-code-javabuilder/theater/src/main/java/org/code/theater/CatImage.java
+++ org-code-javabuilder/theater/src/main/java/org/code/theater/CatImage.java
@@ -6,10 +6,14 @@ import ij.process.ImageProcessor;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import org.code.protocol.GlobalProtocol;
 import org.code.protocol.OutputAdapter;
+
+import javax.imageio.ImageIO;
 
 /**
  * This is a sample library with some image manipulation that shows how theater could use the
@@ -29,26 +33,28 @@ public class CatImage {
     this.outputStream = new ByteArrayOutputStream();
     this.gifWriter = new GifWriter(this.outputStream);
     this.outputAdapter = outputAdapter;
+    System.setProperty("java.awt.headless", "true");
   }
 
   public void buildImageFilter(int delay) {
-    ImagePlus image = null;
+    Toolkit tk = Toolkit.getDefaultToolkit();
+    File f = null;
+    BufferedImage image = null;
     try {
-      image =
-          IJ.openImage(
-              Paths.get(CatImage.class.getClassLoader().getResource("sampleImageBeach.jpg").toURI())
-                  .toString());
-    } catch (URISyntaxException e) {
+      f = new File(Paths.get(CatImage.class.getClassLoader().getResource("sampleImageBeach.jpg").toURI()).toString());
+      image = ImageIO.read(f);
+    } catch (Exception e) {
       e.printStackTrace();
     }
+//    image.c
 
     // Uncommenting the following line and setting it to a local directory will save the image to
     // your local computer
     // IJ.save(image, "<my local directory>\beach.gif");
 
-    ImageProcessor ip = image.getProcessor();
-    BufferedImage bufferedImage = (BufferedImage) ip.createImage();
-    this.gifWriter.writeToGif(bufferedImage, delay);
+//    ImageProcessor ip = image.getProcessor();
+//    BufferedImage bufferedImage = (BufferedImage) image;
+    this.gifWriter.writeToGif(image, delay);
   }
 
   // Play the created gif (close gif writer and send message).
@@ -58,34 +64,38 @@ public class CatImage {
   }
 
   public void buildCanvas(int delay) {
-    ImagePlus image = IJ.createImage("test", 300, 150, 1, 24);
-    ImageProcessor ip = image.getProcessor();
-
-    // Draw rectangle
-    ip.setLineWidth(10);
-    ip.setColor(Color.BLUE);
-    ip.fillRect(50, 50, 100, 100);
-
-    // Draw square
-    int[] x = {10, 40, 25};
-    int[] y = {10, 10, 30};
-    ip.setColor(Color.RED);
-    ip.fillPolygon(new Polygon(x, y, 3));
-
-    // Draw circle
-    ip.setColor(Color.GREEN);
-    ip.fillOval(30, 20, 50, 30);
-
-    // Draw text
-    ip.setFontSize(30);
-    ip.setColor(Color.YELLOW);
-    ip.drawString("Hello World!", 100, 90);
-
-    // Uncommenting the following line and setting it to a local directory will save the image to
-    // your local computer
-    // IJ.save(image, "<my local directory>\beach.gif");
-
-    BufferedImage bufferedImage = (BufferedImage) ip.createImage();
-    this.gifWriter.writeToGif(bufferedImage, delay);
+//    Canvas canvas = new Canvas();
+//    Image image = canvas.createImage(300, 300);
+//    image.
+//    Graphics ip = image.getGraphics();
+////    ImagePlus image = IJ.createImage("test", 300, 150, 1, 24);
+////    ImageProcessor ip = image.getProcessor();
+//
+//    // Draw rectangle
+////    ip.setLineWidth(10);
+//    ip.setColor(Color.BLUE);
+//    ip.fillRect(50, 50, 100, 100);
+//
+//    // Draw square
+//    int[] x = {10, 40, 25};
+//    int[] y = {10, 10, 30};
+//    ip.setColor(Color.RED);
+//    ip.fillPolygon(new Polygon(x, y, 3));
+//
+//    // Draw circle
+//    ip.setColor(Color.GREEN);
+//    ip.fillOval(30, 20, 50, 30);
+//
+//    // Draw text
+////    ip.setFontSize(30);
+//    ip.setFont(new Font("Arial", Font.ITALIC, 12));
+//    ip.drawString("Test", 32, 8);
+//
+//    // Uncommenting the following line and setting it to a local directory will save the image to
+//    // your local computer
+//    // IJ.save(image, "<my local directory>\beach.gif");
+//
+////    BufferedImage bufferedImage = (BufferedImage) ip.createImage();
+//    this.gifWriter.writeToGif((BufferedImage)image, delay);
   }
 }
