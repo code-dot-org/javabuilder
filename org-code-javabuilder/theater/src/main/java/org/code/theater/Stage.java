89235fddb34fbc43e1a69035246ca1067c02058f
molly-moen <molly@code.org>
2021-06-24 10:58:52 -0700

    first pass at shapes

--- org-code-javabuilder/theater/src/main/java/org/code/theater/Stage.java
+++ org-code-javabuilder/theater/src/main/java/org/code/theater/Stage.java
@@ -1,17 +1,47 @@
 package org.code.theater;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import org.code.media.*;
+import org.code.media.Color;
+import org.code.media.Image;
+import org.code.protocol.GlobalProtocol;
+import org.code.protocol.OutputAdapter;
 
 public class Stage {
+  private BufferedImage image;
+  private final OutputAdapter outputAdapter;
+  private final GifWriter gifWriter;
+  private final ByteArrayOutputStream outputStream;
+  private java.awt.Color strokeColor;
+  private java.awt.Color fillColor;
+  private Graphics2D graphics;
+  private boolean hasPlayed;
+
+  private static final int WIDTH = 400;
+  private static final int HEIGHT = 400;
+
+  public Stage() {
+    this.outputAdapter = GlobalProtocol.getInstance().getOutputAdapter();
+    this.outputStream = new ByteArrayOutputStream();
+    this.gifWriter = new GifWriter(this.outputStream);
+
+    // Set up image with white background and black stroke/fill color
+    this.reset();
+    this.hasPlayed = false;
+
+    System.setProperty("java.awt.headless", "true");
+  }
+
   /** Returns the width of the theater canvas. Right now this will always be 400 pixels. */
   public double getWidth() {
-    return -1;
+    return image.getWidth();
   }
 
   /** Returns the height of the theater canvas. Right now this will always be 400 pixels. */
   public double getHeight() {
-    return -1;
+    return image.getHeight();
   }
 
   /**
@@ -45,13 +75,23 @@ public class Stage {
    * @param seconds The number of seconds to wait. This can be a fraction of a second, but the
    *     smallest value can be .1 seconds.
    */
-  public void wait(double seconds) {}
+  public void wait(double seconds) {
+    this.gifWriter.writeToGif(this.image, (int) (seconds * 1000));
+  }
 
   /** Clear everything, starting from the beginning and emptying the canvas. */
-  public void reset() {}
+  public void reset() {
+    this.image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    this.graphics = this.image.createGraphics();
+    this.graphics.setBackground(java.awt.Color.WHITE);
+    // clearRect removes the default black background
+    this.graphics.clearRect(0, 0, WIDTH, HEIGHT);
+    this.strokeColor = java.awt.Color.BLACK;
+    this.fillColor = java.awt.Color.BLACK;
+  }
 
   /**
-   * Draw an image on the canvas at the given location, expanded or shunk to fit the width and
+   * Draw an image on the canvas at the given location, expanded or shrunk to fit the width and
    * height provided
    *
    * @param filename the name of the file in the asset manager
@@ -62,12 +102,11 @@ public class Stage {
    * @param rotation the amount to rotate the image
    * @throws FileNotFoundException if the file can't be found in the project.
    */
-  public void drawImage(
-      String filename, double x, double y, double width, double height, double rotation)
+  public void drawImage(String filename, int x, int y, int width, int height, double rotation)
       throws FileNotFoundException {}
 
   /**
-   * Draw an image on the canvas at the given location, expanded or shunk to fit the width and
+   * Draw an image on the canvas at the given location, expanded or shrunk to fit the width and
    * height provided
    *
    * @param image the Image object to draw on the canvas
@@ -77,8 +116,7 @@ public class Stage {
    * @param height the height to draw the image on the canvas
    * @param rotation the amount to rotate the image
    */
-  public void drawImage(
-      Image image, double x, double y, double width, double height, double rotation) {}
+  public void drawImage(Image image, int x, int y, int width, int height, double rotation) {}
 
   /**
    * Draws text on the image.
@@ -92,7 +130,7 @@ public class Stage {
    * @param rotation the rotation or tilt of the text, in degrees
    */
   public void drawText(
-      String text, double x, double y, String color, String font, double height, double rotation) {}
+      String text, int x, int y, String color, String font, int height, double rotation) {}
 
   /**
    * Draw a line on the canvas.
@@ -102,7 +140,14 @@ public class Stage {
    * @param endX the end X coordinate of the line.
    * @param endY the end Y coordinate of the line.
    */
-  public void drawLine(double startX, double startY, double endX, double endY) {}
+  public void drawLine(int startX, int startY, int endX, int endY) {
+    if (this.strokeColor == null) {
+      this.graphics.setColor(java.awt.Color.BLACK);
+    } else {
+      this.graphics.setColor(this.strokeColor);
+    }
+    this.graphics.drawLine(startX, startY, endX, endY);
+  }
 
   /**
    * Draw a regular polygon on the canvas.
@@ -112,7 +157,25 @@ public class Stage {
    * @param sides the number of sides of the polygon
    * @param radius the distance from the center to each point on the polygon
    */
-  public void drawRegularPolygon(double x, double y, int sides, double radius) {}
+  public void drawRegularPolygon(int x, int y, int sides, int radius) {
+    if (this.strokeColor != null || this.fillColor != null) {
+      Polygon polygon = new Polygon();
+      double theta = 2 * Math.PI / sides;
+      for (int i = 0; i < sides; i++) {
+        int xCoordinate = (int) (Math.cos(theta * i) * radius) + x;
+        int yCoordinate = (int) (Math.sin(theta * i) * radius) + y;
+        polygon.addPoint(xCoordinate, yCoordinate);
+      }
+      if (this.strokeColor != null) {
+        this.graphics.setColor(this.strokeColor);
+        this.graphics.drawPolygon(polygon);
+      }
+      if (this.fillColor != null) {
+        this.graphics.setColor(this.fillColor);
+        this.graphics.fillPolygon(polygon);
+      }
+    }
+  }
 
   /**
    * Draw as a shape by connecting the points provided.
@@ -123,7 +186,40 @@ public class Stage {
    *     point will be connected by a line, and if a fill color is set, the shape will be filled
    *     with that color.
    */
-  public void drawShape(double[] points, boolean close) {}
+  public void drawShape(int[] points, boolean close) {
+    // points should contain an even number of values to represent (x,y) coordinates
+    // We need at least 4 points to draw a single line
+    if (points.length % 2 != 0 || points.length < 4) {
+      throw new TheaterRuntimeException(ExceptionKeys.INVALID_SHAPE);
+    }
+    if (close) {
+      // we can use the shape functions
+      if (this.strokeColor != null || this.fillColor != null) {
+        int numPoints = points.length / 2;
+        int[] xPoints = new int[numPoints];
+        int[] yPoints = new int[numPoints];
+        // split points into x and y arrays
+        for (int i = 0; i < points.length - 1; i += 2) {
+          xPoints[i / 2] = points[i];
+          yPoints[i / 2] = points[i + 1];
+        }
+        if (this.strokeColor != null) {
+          this.graphics.setColor(this.strokeColor);
+          this.graphics.drawPolygon(xPoints, yPoints, numPoints);
+        }
+        if (this.fillColor != null) {
+          this.graphics.setColor(this.fillColor);
+          this.graphics.fillPolygon(xPoints, yPoints, numPoints);
+        }
+      }
+    } else if (this.strokeColor != null) {
+      // draw shape as a series of lines
+      this.graphics.setColor(this.strokeColor);
+      for (int i = 0; i <= points.length - 4; i += 2) {
+        this.graphics.drawLine(points[i], points[i + 1], points[i + 2], points[i + 3]);
+      }
+    }
+  }
 
   /**
    * Draws an ellipse (an oval or a circle) on the canvas.
@@ -133,7 +229,16 @@ public class Stage {
    * @param width the width of the ellipse
    * @param height the height of the ellipse
    */
-  public void drawEllipse(double x, double y, double width, double height) {}
+  public void drawEllipse(int x, int y, int width, int height) {
+    if (this.fillColor != null) {
+      this.graphics.setColor(this.fillColor);
+      this.graphics.fillOval(x, y, width, height);
+    }
+    if (this.strokeColor != null) {
+      this.graphics.setColor(this.strokeColor);
+      this.graphics.drawOval(x, y, width, height);
+    }
+  }
 
   /**
    * Draws a rectangle on the canvas.
@@ -143,37 +248,67 @@ public class Stage {
    * @param width the width of the rectangle.
    * @param height the height of the rectangle.
    */
-  public void drawRectangle(double x, double y, double width, double height) {}
+  public void drawRectangle(int x, int y, int width, int height) {
+    if (this.fillColor != null) {
+      this.graphics.setColor(this.fillColor);
+      this.graphics.fillRect(x, y, width, height);
+    }
+    if (this.strokeColor != null) {
+      this.graphics.setColor(this.strokeColor);
+      this.graphics.drawRect(x, y, width, height);
+    }
+  }
 
   /**
    * Sets the thickness of lines drawn.
    *
    * @param width width in pixels of the line to draw. Zero means no line.
    */
-  public void setStrokeWidth(double width) {}
+  public void setStrokeWidth(double width) {
+    this.graphics.setStroke(new BasicStroke((float) width));
+  }
 
   /**
    * Sets the color of lines drawn.
    *
    * @param color the color to draw lines with.
    */
-  public void setStrokeColor(Color color) {}
+  public void setStrokeColor(Color color) {
+    this.strokeColor = this.convertColor(color);
+  }
 
   /** Removes the stroke color so any shapes have no outlines. */
-  public void removeStrokeColor() {}
+  public void removeStrokeColor() {
+    this.strokeColor = null;
+  }
 
   /**
    * Sets the fill color for all shapes drawn
    *
    * @param color the color value to fill any shape.
    */
-  public void setFillColor(Color color) {}
+  public void setFillColor(Color color) {
+    this.fillColor = this.convertColor(color);
+  }
 
   /** Removes the fill color so any shapes have no fill. */
-  public void removeFillColor() {}
+  public void removeFillColor() {
+    this.fillColor = null;
+  }
 
   /** Plays the instructions. */
   public void play() {
-    System.out.println("play");
+    if (this.hasPlayed) {
+      throw new TheaterRuntimeException(ExceptionKeys.DUPLICATE_PLAY_COMMAND);
+    } else {
+      this.gifWriter.writeToGif(this.image, 0);
+      this.gifWriter.close();
+      outputAdapter.sendMessage(ImageEncoder.encodeStreamToMessage(this.outputStream));
+      this.hasPlayed = true;
+    }
+  }
+
+  private java.awt.Color convertColor(Color color) {
+    return new java.awt.Color(color.getRed(), color.getGreen(), color.getBlue());
   }
 }
