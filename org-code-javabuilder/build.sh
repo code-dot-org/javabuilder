cc2065e4bb61027e49caf936d44609e6afb7ff63
Suresh Chanmugam <me@suresh.com>
2021-06-24 11:38:18 -0700

    Fix comment.

--- org-code-javabuilder/build.sh
+++ org-code-javabuilder/build.sh
@@ -3,5 +3,5 @@
 # cd to script directory (shh, it's magic: https://stackoverflow.com/questions/6393551/what-is-the-meaning-of-0-in-a-bash-script)
 cd "${0%/*}"
 
-# Build a zip package that can be uploaded to AWS Lambda.
+# Build a zip package that can be uploaded to AWS Lambda by CloudFormation.
 ./gradlew buildZip
