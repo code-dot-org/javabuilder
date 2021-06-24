e4e2b1e5f1911d73777e66ca2f50510cba86cff6
Sanchit Malhotra <sanchit@code.org>
2021-06-24 12:58:28 -0700

    Add initial audio read/write capabilities to Theater (WAV only)

--- org-code-javabuilder/media/src/main/java/org/code/media/SoundException.java
+++ org-code-javabuilder/media/src/main/java/org/code/media/SoundException.java
@@ -1,3 +1,19 @@
 package org.code.media;
 
-public class SoundException extends Exception {}
+public class SoundException extends Exception {
+  public SoundException() {
+    super();
+  }
+
+  public SoundException(String message) {
+    super(message);
+  }
+
+  public SoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public SoundException(Throwable cause) {
+    super(cause);
+  }
+}
--- org-code-javabuilder/media/src/main/java/org/code/media/SoundLoader.java
+++ org-code-javabuilder/media/src/main/java/org/code/media/SoundLoader.java
@@ -1,5 +1,12 @@
 package org.code.media;
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class SoundLoader {
   /**
    * Extracts audio samples from WAV or AU file into array
@@ -10,6 +17,43 @@ public class SoundLoader {
    * @throws SoundException when the file cannot be found
    */
   public static double[] read(String filename) throws SoundException {
-    return null;
+    // Acquire AudioInputStream
+    final AudioInputStream audioInputStream = getAudioInputStream(filename);
+    final AudioFormat audioFormat = audioInputStream.getFormat();
+    if (!AudioUtils.isAudioFormatValid(audioFormat)) {
+      throw new SoundException("Invalid audio file format");
+    }
+
+    // Read audio file samples as bytes
+    final byte[] bytes;
+    try {
+      final int bytesToRead = audioInputStream.available();
+      bytes = new byte[bytesToRead];
+      final int bytesRead = audioInputStream.read(bytes);
+      if (bytesToRead != bytesRead) {
+        throw new SoundException("Error reading audio file");
+      }
+      audioInputStream.close();
+    } catch (IOException e) {
+      throw new SoundException(e);
+    }
+
+    return AudioUtils.convertByteArrayToDoubleArray(bytes, audioFormat.getChannels());
+  }
+
+  private static AudioInputStream getAudioInputStream(String filename) throws SoundException {
+    if (filename == null) {
+      throw new SoundException("Missing file name");
+    }
+
+    try {
+      final File file = new File(filename);
+      if (file.exists()) {
+        return AudioSystem.getAudioInputStream(file);
+      }
+      throw new SoundException("File does not exist: " + filename);
+    } catch (IOException | UnsupportedAudioFileException e) {
+      throw new SoundException(e);
+    }
   }
 }
