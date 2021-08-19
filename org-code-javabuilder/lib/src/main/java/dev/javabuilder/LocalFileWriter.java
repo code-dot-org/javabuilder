5bd1294c59f5fa09d7896a6128c02e28a3bd3719
Jessica Kulwik <jessica@code.org>
2021-08-19 15:57:04 -0700

    Updated to be more secure

--- org-code-javabuilder/lib/src/main/java/dev/javabuilder/LocalFileWriter.java
+++ org-code-javabuilder/lib/src/main/java/dev/javabuilder/LocalFileWriter.java
@@ -1,8 +1,11 @@
 package dev.javabuilder;
 
+import static dev.javabuilder.LocalWebserverConstants.DIRECTORY;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.code.javabuilder.UserFacingException;
 import org.code.protocol.InternalErrorKey;
 import org.code.protocol.JavabuilderException;
@@ -12,12 +15,16 @@ public class LocalFileWriter implements JavabuilderFileWriter {
   @Override
   public String writeToFile(String filename, byte[] inputBytes, String contentType)
       throws JavabuilderException {
-    File file = new File(filename);
+    File file = Paths.get(System.getProperty("java.io.tmpdir"), DIRECTORY, filename).toFile();
     try {
+      File parentDirectory = Paths.get(System.getProperty("java.io.tmpdir"), DIRECTORY).toFile();
+      if (!parentDirectory.exists()) {
+        parentDirectory.mkdirs();
+      }
       Files.write(file.toPath(), inputBytes);
     } catch (IOException e) {
       throw new UserFacingException(InternalErrorKey.INTERNAL_RUNTIME_EXCEPTION, e);
     }
-    return "http://localhost:8080/files/" + filename;
+    return String.format("http://localhost:8080/%s/%s", DIRECTORY, filename);
   }
 }
