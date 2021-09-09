bc36d349bee57cdff22e924584769857171e07b5
Michael <moneppo@users.noreply.github.com>
2021-09-09 09:55:42 -0700

    Update and rename ClickableImage.java to ClickableItem.java

--- org-code-javabuilder/playground/src/main/java/org/code/playground/ClickableItem.java
+++ org-code-javabuilder/playground/src/main/java/org/code/playground/ClickableItem.java
@@ -2,12 +2,10 @@ package org.code.playground;
 
 import java.io.FileNotFoundException;
 
-import org.code.media.Image;
-
-public abstract class ClickableImage extends Image {
+public abstract class ClickableItem extends Item {
 
     /**
-     * Sets a sound to play when the user clicks on this image.
+     * Sets a sound to play when the user clicks on this item.
      * 
      * @param filename the name of the sound file from the asset manager to play
      * @throws FileNotFoundException when the sound file cannot be found.
@@ -15,8 +13,8 @@ public abstract class ClickableImage extends Image {
     public void setClickSound(String filename) throws FileNotFoundException;
 
     /**
-     * Called when this image is clicked. Implement this method in a subclass to
-     * control what happens when the user clicks on the image.
+     * Called when this item is clicked. Implement this method in a subclass to
+     * control what happens when the user clicks on the item.
      */
     public abstract void onClick();
 }
