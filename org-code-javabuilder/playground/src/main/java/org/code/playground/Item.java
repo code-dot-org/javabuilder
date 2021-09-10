08c37239c7598a2b487af5fe45353b5bbbf1c55d
Michael <moneppo@users.noreply.github.com>
2021-09-10 15:56:51 -0700

    Move x, y, width, and height as properties to Item

--- org-code-javabuilder/playground/src/main/java/org/code/playground/Item.java
+++ org-code-javabuilder/playground/src/main/java/org/code/playground/Item.java
@@ -4,19 +4,80 @@ import java.io.FileNotFoundException;
 
 public class Item {
   
-   /**
-   * Creates an item that can be displayed in the Playground. An item simply consists of an image,
-   * referenced by the name of the image file in the asset manager.
+  /**
+   * Creates an item that can be displayed in the Playground. An item consists of an image,
+   * referenced by the name of the image file in the asset manager. The image for the item will
+   * be scaled to fit the width and height provided, which may distort the image.
    *
-   * @param filename the string name of the color (case insensitive).
+   * @param filename the file name of the image for this item in the asset manager
+   * @param x the distance, in pixels, from the left side of the board
+   * @param y the distance, in pixels, from the top of the board
+   * @param width the width of the item, in pixels
+   * @param height the height of the item, in pixels
    * @throws FileNotFoundException if the file specified is not the in the asset manager
    */
-  public Item(String filename) throws FileNotFoundException;
+  public Item(String filename, int x, int y, int width, int height) throws FileNotFoundException;
   
   /**
-  * Retrieve the filename for this item.
-  *
-  * @return the filename in the asset manager for the image associated with this item.
-  */
+   * Retrieve the filename for this item.
+   *
+   * @return the filename in the asset manager for the image associated with this item.
+   */
   public String getFilename();
+  
+  /**
+   * Set the X position for the item.
+   *
+   * @param x the distance, in pixels, from the left side of the board
+   */
+  public void setX(int x);
+  
+  /**
+   * Get the X position for the item.
+   *
+   * @return the distance, in pixels, from the left side of the board
+   */
+  public int getX();
+  
+  /**
+   * Set the Y position for the item.
+   *
+   * @param y the distance, in pixels, from the top of the board
+   */
+  public void setY(int y);
+
+  /**
+   * Get the Y position for the item.
+   *
+   * @return the distance, in pixels, from the top of the board
+   */
+  public int getY();
+  
+  /**
+   * Set the width for the item.
+   *
+   * @param width the width of the item, in pixels
+   */
+  public void setWidth(int width);
+
+  /**
+   * Get the width for the item.
+   *
+   * @return the width of the item, in pixels
+   */
+  public int getWidth();
+  
+  /**
+   * Set the height for the item.
+   *
+   * @param height the height of the item, in pixels
+   */
+  public void setHeight(int height);
+  
+  /**
+   * Get the height for the item.
+   *
+   * @return the height of the item, in pixels
+   */
+  public int getHeight();
 }
