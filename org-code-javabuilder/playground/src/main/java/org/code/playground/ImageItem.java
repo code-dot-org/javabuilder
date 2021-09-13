d1dc96669e07c2c634bf899534c798f3da01ac9b
Michael <moneppo@users.noreply.github.com>
2021-09-13 14:58:46 -0700

    Update ImageItem.java

--- org-code-javabuilder/playground/src/main/java/org/code/playground/ImageItem.java
+++ org-code-javabuilder/playground/src/main/java/org/code/playground/ImageItem.java
@@ -2,7 +2,7 @@ package org.code.playground;
 
 import java.io.FileNotFoundException;
 
-public class ImageItem extends Item{
+public class ImageItem implements Item{
   
   /**
    * Creates an item that can be displayed in the Playground. An item consists of an image,
@@ -32,4 +32,60 @@ public class ImageItem extends Item{
    * @throws FileNotFoundException if the file specified is not the in the asset manager
    */
   public void getFilename(String filename) throws FileNotFoundException;
+  
+   /**
+   * Set the X position for the item.
+   *
+   * @param x the distance, in pixels, from the left side of the board
+   */
+  public void setX(int x);
+  
+  /**
+   * Get the X position for the item.
+   *
+   * @return the distance, in pixels, from the left side of the board
+   */
+  public int getX();
+  
+  /**
+   * Set the Y position for the item.
+   *
+   * @param y the distance, in pixels, from the top of the board
+   */
+  public void setY(int y);
+
+  /**
+   * Get the Y position for the item.
+   *
+   * @return the distance, in pixels, from the top of the board
+   */
+  public int getY();
+  
+  /**
+   * Set the width for the item.
+   *
+   * @param width the width of the item, in pixels
+   */
+  public void setWidth(int width);
+
+  /**
+   * Get the width for the item.
+   *
+   * @return the width of the item, in pixels
+   */
+  public int getWidth();
+  
+  /**
+   * Set the height for the item.
+   *
+   * @param height the height of the item, in pixels
+   */
+  public void setHeight(int height);
+  
+  /**
+   * Get the height for the item.
+   *
+   * @return the height of the item, in pixels
+   */
+  public int getHeight();
 }
