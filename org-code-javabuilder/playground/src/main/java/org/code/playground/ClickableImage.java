2bf73b80063622d1279af2b488760d54b4a4177b
Michael <moneppo@users.noreply.github.com>
2021-09-14 14:08:26 -0700

    Update ClickableImage.java

--- org-code-javabuilder/playground/src/main/java/org/code/playground/ClickableImage.java
+++ org-code-javabuilder/playground/src/main/java/org/code/playground/ClickableImage.java
@@ -3,15 +3,6 @@ package org.code.playground;
 import java.io.FileNotFoundException;
 
 public abstract class ClickableImage extends ImageItem {
-
-    /**
-     * Sets a sound to play when the user clicks on this item.
-     * 
-     * @param filename the name of the sound file from the asset manager to play
-     * @throws FileNotFoundException when the sound file cannot be found.
-     */
-    public void setClickSound(String filename) throws FileNotFoundException;
-
     /**
      * Called when this item is clicked. Implement this method in a subclass to
      * control what happens when the user clicks on the item.
