67a90630fefc6af9fc096f385e257623f346364b
Sanchit Malhotra <sanchit@code.org>
2021-09-20 16:03:15 -0700

    Create Playground Message Handler

---
+++ org-code-javabuilder/playground/src/test/java/org/code/playground/PlaygroundMessageHandlerTest.java
@@ -0,0 +1,31 @@
+package org.code.playground;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.HashMap;
+import org.code.protocol.OutputAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlaygroundMessageHandlerTest {
+
+  private OutputAdapter outputAdapter;
+  private PlaygroundMessageHandler unitUnderTest;
+
+  @BeforeEach
+  public void setUp() {
+    outputAdapter = mock(OutputAdapter.class);
+    unitUnderTest = new PlaygroundMessageHandler(outputAdapter);
+  }
+
+  @Test
+  public void testSendMessageCallsOutputAdapter() {
+    final PlaygroundMessage message =
+        new PlaygroundMessage(PlaygroundSignalKey.RUN, new HashMap<>());
+
+    unitUnderTest.sendMessage(message);
+
+    verify(outputAdapter).sendMessage(message);
+  }
+}
