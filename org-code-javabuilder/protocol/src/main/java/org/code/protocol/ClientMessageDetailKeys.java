52cfc6aca22f01924b1e70fd8e8ebe65ed3091d2
Sanchit Malhotra <sanchit@code.org>
2021-09-21 15:53:45 -0700

    Remove final

--- org-code-javabuilder/protocol/src/main/java/org/code/protocol/ClientMessageDetailKeys.java
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/ClientMessageDetailKeys.java
@@ -1,6 +1,6 @@
 package org.code.protocol;
 
 /** Expected keys in the optional detail object of {@link ClientMessage}s */
-public final class ClientMessageDetailKeys {
+public class ClientMessageDetailKeys {
   public static final String FILENAME = "filename";
 }
