66a009ec4dbed920779de7e9c2667b1d172d5813
Suresh Chanmugam <me@suresh.com>
2021-10-15 13:41:36 -0700

    Remove double build now that gradle jar issue has been fixed.

--- org-code-javabuilder/build.sh
+++ org-code-javabuilder/build.sh
@@ -11,6 +11,3 @@ zip -r font_config.zip fontconfig.properties
 
 # Build a zip package that can be uploaded to AWS Lambda by CloudFormation.
 ./gradlew buildZip
-# Build a second time to workaround an issue where the jars directory does not exist yet.
-# https://codedotorg.slack.com/archives/C01EF4GJ9GE/p1634164567122400?thread_ts=1634148875.098800&channel=C01EF4GJ9GE&message_ts=1634164567.122400
-./gradlew buildZip
