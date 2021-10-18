fbd2d18d1867f0cf1fa64b0a3c047103d426b027
Ben Brooks <ben@code.org>
2021-10-18 16:24:54 -0700

    Correct method signature based on spec

--- org-code-javabuilder/theater/src/main/java/org/code/theater/Prompter.java
+++ org-code-javabuilder/theater/src/main/java/org/code/theater/Prompter.java
@@ -1,12 +1,11 @@
 package org.code.theater;
 
-import java.io.FileNotFoundException;
 import org.code.media.Image;
 
 public class Prompter {
   protected Prompter() {}
 
-  public Image getImage(String filename) throws FileNotFoundException {
-    return new Image(filename);
+  public Image getImage(String prompt) {
+    return null;
   }
 }
