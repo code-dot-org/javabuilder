0a8b5f49800921c615103508124bf9ecaf8ffdb2
molly-moen <molly@code.org>
2021-10-20 13:37:40 -0700

    fix comments

--- org-code-javabuilder/protocol/src/main/java/org/code/protocol/InputAdapter.java
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/InputAdapter.java
@@ -4,5 +4,9 @@ public interface InputAdapter {
   /** @return The next user input to the currently running program */
   String getNextMessage();
 
+  /**
+   * @return If there is still an active connection to the input. If false is returned, it will
+   *     never return true again.
+   */
   boolean hasActiveConnection();
 }
