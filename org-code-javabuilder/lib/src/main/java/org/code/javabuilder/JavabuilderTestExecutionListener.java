a335abed495688ef7c08175444af98a103ea6c14
Sanchit Malhotra <sanchit@code.org>
2021-10-22 16:31:20 -0500

    add docs

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavabuilderTestExecutionListener.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/JavabuilderTestExecutionListener.java
@@ -11,6 +11,16 @@ import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
+/**
+ * A {@link org.junit.platform.launcher.TestExecutionListener} that posts test status and results to
+ * the provided {@link OutputAdapter}. Since this class only implements a subset of
+ * TestExecutionListener methods, it extends {@link SummaryGeneratingListener} and defers any
+ * additional functionality to it.
+ *
+ * <p>See:
+ * https://junit.org/junit5/docs/5.0.0/api/org/junit/platform/launcher/listeners/SummaryGeneratingListener.html
+ * https://junit.org/junit5/docs/5.0.0/api/org/junit/platform/launcher/TestExecutionListener.html
+ */
 public class JavabuilderTestExecutionListener extends SummaryGeneratingListener {
   private final OutputAdapter outputAdapter;
 
@@ -19,11 +29,42 @@ public class JavabuilderTestExecutionListener extends SummaryGeneratingListener 
     this.outputAdapter = outputAdapter;
   }
 
+  /**
+   * Called when the execution of the TestPlan has started, before any test has been executed. This
+   * listener sends a message via the OutputAdapter to indicate that code is running.
+   *
+   * <p>See:
+   * https://junit.org/junit5/docs/5.0.0/api/org/junit/platform/launcher/TestExecutionListener.html#testPlanExecutionStarted-org.junit.platform.launcher.TestPlan-
+   *
+   * @param testPlan describes the tree of tests that have been executed
+   */
   public void testPlanExecutionStarted(TestPlan testPlan) {
     super.testPlanExecutionStarted(testPlan);
     this.outputAdapter.sendMessage(new StatusMessage(StatusMessageKey.RUNNING));
   }
 
+  /**
+   * Called when the execution of a leaf or subtree of the TestPlan has finished, regardless of the
+   * outcome. This listener publishes the test result and, if the test failed, the error message to
+   * the OutputAdapter.
+   *
+   * <p>The format of the test result is: [test class name] > [test name] [test result]
+   *
+   * <p>for example:
+   *
+   * <p>MyTestClass > myTest SUCCEEDED
+   *
+   * <p>MyTestClass > myTest FAILED
+   *
+   * <p>failure message (MyTestClass:1)
+   *
+   * <p>See:
+   * https://junit.org/junit5/docs/5.0.0/api/org/junit/platform/launcher/TestExecutionListener.html#executionFinished-org.junit.platform.launcher.TestIdentifier-org.junit.platform.engine.TestExecutionResult-
+   *
+   * @param testIdentifier the identifier of the finished test or container
+   * @param testExecutionResult the (unaggregated) result of the execution for the supplied
+   *     TestIdentifier
+   */
   public void executionFinished(
       TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
     super.executionFinished(testIdentifier, testExecutionResult);
@@ -50,6 +91,26 @@ public class JavabuilderTestExecutionListener extends SummaryGeneratingListener 
     }
   }
 
+  /**
+   * Generates an error message for a failing test given the Throwable and class name associated
+   * with the test.
+   *
+   * <p>If the test threw an {@link AssertionError} (i.e. an assertion in the test failed), then the
+   * format of the error message is: "[error message] ([java class name]:[line number])"
+   *
+   * <p>If the test threw another exception, then format of the error message is: "[exception name]
+   * thrown at [java class name]:[line number]"
+   *
+   * <p>For example:
+   *
+   * <p>assertion failed (MyTest:1)
+   *
+   * <p>FileNotFoundException thrown at MyTest:10
+   *
+   * @param throwable Throwable thrown by the test
+   * @param className name of the test class
+   * @return the error message
+   */
   private String getErrorMessage(Throwable throwable, String className) {
     final String errorLine = this.errorLine(throwable.getStackTrace(), className);
     final String errorMessage;
@@ -68,7 +129,7 @@ public class JavabuilderTestExecutionListener extends SummaryGeneratingListener 
 
   /**
    * Finds the stack trace element containing the class name and returns the line number of the
-   * error
+   * error.
    *
    * @param stackTrace array of stack trace elements
    * @param className name of the test class
