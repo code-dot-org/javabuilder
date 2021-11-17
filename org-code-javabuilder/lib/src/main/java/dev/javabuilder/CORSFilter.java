5cbfb27cd487d9f9387402fcbdaaf5325105544b
Sanchit Malhotra <sanchit@code.org>
2021-11-17 16:59:19 -0500

    Enable prompter on localhost

---
+++ org-code-javabuilder/lib/src/main/java/dev/javabuilder/CORSFilter.java
@@ -0,0 +1,53 @@
+package dev.javabuilder;
+
+import static dev.javabuilder.LocalWebserverConstants.DIRECTORY;
+import static org.code.protocol.AllowedFileNames.PROMPTER_FILE_NAME_PREFIX;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A {@link HttpFilter} for local development that upgrades specific known and trusted requests by
+ * adding necessary CORS headers. In production, this is handled by S3.
+ *
+ * <p>https://www.oracle.com/java/technologies/filters.html
+ */
+@WebFilter(urlPatterns = {"/" + DIRECTORY + "/*"})
+public class CORSFilter extends HttpFilter {
+
+  private static final List<String> ALLOWED_ORIGINS =
+      List.of("http://localhost-studio.code.org:3000", "http://127.0.0.1:3000");
+
+  @Override
+  public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    final String requestOrigin = request.getHeader("Origin");
+    if (requestOrigin == null || !ALLOWED_ORIGINS.contains(requestOrigin)) {
+      // May be an internal request or an unknown origin. Pass along the request without adding any
+      // headers.
+      chain.doFilter(request, response);
+      return;
+    }
+
+    final String[] urlParts = request.getRequestURI().split("/");
+    final String fileName = urlParts[urlParts.length - 1];
+    if (fileName.indexOf(PROMPTER_FILE_NAME_PREFIX) == 0) {
+      // Add CORS headers only if the request is for a known file
+      response.addHeader("Access-Control-Allow-Origin", requestOrigin);
+      response.addHeader("Access-Control-Allow-Headers", "*");
+      response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PUT");
+    }
+
+    if (request.getMethod().equals("OPTIONS")) {
+      response.setStatus(HttpServletResponse.SC_ACCEPTED);
+      return;
+    }
+
+    chain.doFilter(request, response);
+  }
+}
--- org-code-javabuilder/protocol/src/main/java/org/code/protocol/AllowedFileNames.java
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/AllowedFileNames.java
@@ -3,4 +3,5 @@ package org.code.protocol;
 public class AllowedFileNames {
   public static final String THEATER_IMAGE_NAME = "theaterImage.gif";
   public static final String THEATER_AUDIO_NAME = "theaterAudio.wav";
+  public static final String PROMPTER_FILE_NAME_PREFIX = "prompterImage-";
 }
