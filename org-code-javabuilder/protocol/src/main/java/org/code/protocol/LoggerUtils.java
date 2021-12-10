fcaebe70c0916ce3b4649cecedbf539c77f9a667
molly-moen <molly@code.org>
2021-12-10 13:00:09 -0800

    clean up

--- org-code-javabuilder/protocol/src/main/java/org/code/protocol/LoggerUtils.java
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/LoggerUtils.java
@@ -6,28 +6,9 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Logger;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class LoggerUtils {
-  public static void sendWarningForException(Exception e) {
-    JSONObject eventData = new JSONObject();
-    eventData.put(LoggerConstants.EXCEPTION_MESSAGE, e.getMessage());
-    eventData.put(LoggerConstants.TYPE, e.getClass().getSimpleName());
-    Logger.getLogger(MAIN_LOGGER).warning(eventData.toString());
-  }
-
-  public static void sendFileNameList() {
-    File f = Paths.get(System.getProperty("java.io.tmpdir")).toFile();
-    JSONObject eventData = new JSONObject();
-    JSONArray array = new JSONArray();
-    String[] pathnames = f.list();
-    array.putAll(pathnames);
-    eventData.put(LoggerConstants.TYPE, "filesInTmp");
-    eventData.put("fileNames", array);
-    Logger.getLogger(MAIN_LOGGER).info(eventData.toString());
-  }
-
   public static void sendDiskSpaceLogs() {
     File f = Paths.get(System.getProperty("java.io.tmpdir")).toFile();
     JSONObject eventData = new JSONObject();
@@ -37,7 +18,6 @@ public class LoggerUtils {
     eventData.put(LoggerConstants.FREE_SPACE, f.getFreeSpace());
     eventData.put(LoggerConstants.TOTAL_SPACE, f.getTotalSpace());
     Logger.getLogger(MAIN_LOGGER).info(eventData.toString());
-    sendFileNameList();
   }
 
   public static void sendClearedDirectoryLog(Path p) {
