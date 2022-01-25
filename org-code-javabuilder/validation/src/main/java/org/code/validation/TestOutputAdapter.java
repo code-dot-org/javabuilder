395ea5036937759318d7753093dce6021ca093de
molly-moen <molly@code.org>
2022-01-24 17:04:13 -0800

    implement TestOutputAdapter

--- org-code-javabuilder/validation/src/main/java/org/code/validation/TestOutputAdapter.java
+++ org-code-javabuilder/validation/src/main/java/org/code/validation/TestOutputAdapter.java
@@ -1,11 +1,43 @@
 package org.code.validation;
 
 import org.code.protocol.ClientMessage;
+import org.code.protocol.ClientMessageType;
 import org.code.protocol.OutputAdapter;
 
 public class TestOutputAdapter implements OutputAdapter {
+  private boolean isValidation;
+  private OutputAdapter delegateOutputAdapter;
+
+  public TestOutputAdapter(OutputAdapter delegateOutputAdapter) {
+    this.isValidation = false;
+    this.delegateOutputAdapter = delegateOutputAdapter;
+  }
+
   @Override
   public void sendMessage(ClientMessage message) {
     // TODO: write method
+    ClientMessageType messageType = message.getType();
+    switch (messageType) {
+        // TODO: when we have a TEST_RESULT type send that on to the delegate as well
+      case STATUS:
+        delegateOutputAdapter.sendMessage(message);
+        break;
+      case SYSTEM_OUT:
+        if (!this.isValidation) {
+          delegateOutputAdapter.sendMessage(message);
+        }
+        break;
+      case NEIGHBORHOOD:
+        if (this.isValidation) {
+          // TODO: parse neighborhood messages in a validation run
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  public void setIsValidation(boolean isValidation) {
+    this.isValidation = isValidation;
   }
 }
---
+++ org-code-javabuilder/validation/src/test/java/org/code/validation/TestOutputAdapterTest.java
@@ -0,0 +1,53 @@
+package org.code.validation;
+
+import static org.mockito.Mockito.*;
+
+import org.code.protocol.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestOutputAdapterTest {
+  private final OutputAdapter delegateOutputAdapter = mock(OutputAdapter.class);
+  private TestOutputAdapter testOutputAdapter;
+
+  @BeforeEach
+  public void setUp() {
+    testOutputAdapter = new TestOutputAdapter(delegateOutputAdapter);
+  }
+
+  @Test
+  public void sendsStatusMessages() {
+    StatusMessage statusMessage = new StatusMessage(StatusMessageKey.RUNNING);
+    testOutputAdapter.sendMessage(statusMessage);
+    verify(delegateOutputAdapter).sendMessage(statusMessage);
+  }
+
+  @Test
+  public void sendsSystemOutMessageIfInTestRun() {
+    ClientMessage systemOutMessage =
+        new ClientMessageHelper(ClientMessageType.SYSTEM_OUT, "test system out");
+    testOutputAdapter.setIsValidation(false);
+    testOutputAdapter.sendMessage(systemOutMessage);
+    verify(delegateOutputAdapter).sendMessage(systemOutMessage);
+  }
+
+  @Test
+  public void doesNotSendSystemOutMessageIfInValidationRun() {
+    ClientMessage systemOutMessage =
+        new ClientMessageHelper(ClientMessageType.SYSTEM_OUT, "test system out");
+    testOutputAdapter.setIsValidation(true);
+    testOutputAdapter.sendMessage(systemOutMessage);
+    verify(delegateOutputAdapter, never()).sendMessage(systemOutMessage);
+  }
+
+  @Test
+  public void neverSendsTheaterMessage() {
+    ClientMessage theaterMessage =
+        new ClientMessageHelper(ClientMessageType.THEATER, "test theater");
+    testOutputAdapter.setIsValidation(false);
+    testOutputAdapter.sendMessage(theaterMessage);
+    testOutputAdapter.setIsValidation(true);
+    testOutputAdapter.sendMessage(theaterMessage);
+    verify(delegateOutputAdapter, never()).sendMessage(theaterMessage);
+  }
+}
