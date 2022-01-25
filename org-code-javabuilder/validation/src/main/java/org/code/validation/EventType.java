079c9644a01151cff9b9054ffff757e3b537f072
molly-moen <molly@code.org>
2022-01-25 13:31:17 -0800

    add event type

---
+++ org-code-javabuilder/validation/src/main/java/org/code/validation/EventType.java
@@ -0,0 +1,14 @@
+package org.code.validation;
+
+public enum EventType {
+  INITIALIZE_PAINTER,
+  MOVE,
+  PAINT,
+  REMOVE_PAINT,
+  TAKE_PAINT,
+  HIDE_PAINTER,
+  SHOW_PAINTER,
+  TURN_LEFT,
+  HIDE_BUCKETS,
+  SHOW_BUCKETS
+}
