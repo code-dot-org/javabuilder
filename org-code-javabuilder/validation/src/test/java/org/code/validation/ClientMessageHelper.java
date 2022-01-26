99b71ed4de64ae3099cc413496c85db73d665024
molly-moen <molly@code.org>
2022-01-25 16:19:03 -0800

    add comment

--- org-code-javabuilder/validation/src/test/java/org/code/validation/ClientMessageHelper.java
+++ org-code-javabuilder/validation/src/test/java/org/code/validation/ClientMessageHelper.java
@@ -3,6 +3,7 @@ package org.code.validation;
 import org.code.protocol.ClientMessage;
 import org.code.protocol.ClientMessageType;
 
+// Helper class for testing only to enable simulating multiple ClientMessage types.
 public class ClientMessageHelper extends ClientMessage {
   protected ClientMessageHelper(ClientMessageType type, String value) {
     super(type, value);
