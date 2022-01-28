95cc7d47b7820874876002c2576977956d55191c
Ben Brooks <ben@code.org>
2022-01-28 11:03:24 -0800

    Add TestResultMessage

---
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/TestResultMessage.java
@@ -0,0 +1,11 @@
+package org.code.javabuilder;
+
+import org.code.protocol.ClientMessage;
+import org.code.protocol.ClientMessageType;
+
+/** A message describing a test result directed to the client's terminal. */
+public class TestResultMessage extends ClientMessage {
+  public TestResultMessage(String value) {
+    super(ClientMessageType.TEST_RESULT, value);
+  }
+}
