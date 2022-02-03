a16e21fc663f8145fdfa365007116f709b0ab881
Ben Brooks <ben@code.org>
2022-02-03 15:45:20 -0800

    Move shared exception message methods to helper

---
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/JavabuilderThrowableMessageHelper.java
@@ -0,0 +1,36 @@
+package org.code.protocol;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+/* Helper for formatting exceptions that will be displayed to the user. */
+class JavabuilderThrowableMessageHelper {
+  private JavabuilderThrowableMessageHelper() {}
+
+  static JavabuilderThrowableMessage getExceptionMessage(
+      Throwable throwable, Enum key, String fallbackMessage) {
+    HashMap<String, String> detail = new HashMap<>();
+    detail.put(ClientMessageDetailKeys.CONNECTION_ID, Properties.getConnectionId());
+    if (throwable.getCause() != null) {
+      detail.put(ClientMessageDetailKeys.CAUSE, getLoggingString(throwable));
+      if (throwable.getCause().getMessage() != null) {
+        detail.put(ClientMessageDetailKeys.CAUSE_MESSAGE, throwable.getCause().getMessage());
+      }
+    }
+
+    if (fallbackMessage != null) {
+      detail.put(ClientMessageDetailKeys.FALLBACK_MESSAGE, fallbackMessage);
+    }
+
+    return new JavabuilderThrowableMessage(key, detail);
+  }
+
+  /** @return A pretty version of the exception and stack trace. */
+  static String getLoggingString(Throwable throwable) {
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter printWriter = new PrintWriter(stringWriter);
+    throwable.printStackTrace(printWriter);
+    return stringWriter.toString();
+  }
+}
