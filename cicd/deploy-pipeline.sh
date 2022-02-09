fef5d770d9b1d90a52359f5de9a190b331fed472
Darin Webb <darinlwebb@gmail.com>
2022-02-09 15:13:32 -0600

    initial codebuild setup
    Update pipeline template
    
    remove artifact encryption
    
    update pipelines
    
    poke
    
    poke
    
    test buildspec artifacts
    
    poke
    
    rebase me

---
+++ cicd/deploy-pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+# Create/Update the Javabuilder setup/dependencies stack. This is manually created and maintained, and requires elevated permissions. 
+
+# 'Developer' role requires a specific service role for all CloudFormation operations.
+if [[ $(aws sts get-caller-identity --query Arn --output text) =~ "475661607190:assumed-role/Developer/" ]]; then
+  # Append the role-arn option to the positional parameters $@ passed to cloudformation deploy.
+  set -- "$@" --role-arn "arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/admin/CloudFormationService"
+fi
+
+TEMPLATE_FILE=cicd/2-cicd/pipeline.template.yml
+
+aws cloudformation validate-template \
+  --template-body file://${TEMPLATE_FILE} \
+  | cat
+
+aws cloudformation deploy \
+  --stack-name darin-javabuilder-ci \
+  --template-file ${TEMPLATE_FILE} \
+  --capabilities CAPABILITY_IAM \
+  "$@"
