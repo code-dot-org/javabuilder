e04422629aa29f701fdd302fb8c86924ecb4a56f
Ben Brooks <ben@code.org>
2022-02-14 09:53:05 -0800

    Add comment describing interface

--- org-code-javabuilder/protocol/src/main/java/org/code/protocol/ContentManager.java
+++ org-code-javabuilder/protocol/src/main/java/org/code/protocol/ContentManager.java
@@ -1,5 +1,9 @@
 package org.code.protocol;
 
+/**
+ * Manages content (such as code sources and assets) that are used and generated during a
+ * Javabuilder session.
+ */
 public interface ContentManager {
   byte[] getAssetData(String filename) throws JavabuilderException;
 
