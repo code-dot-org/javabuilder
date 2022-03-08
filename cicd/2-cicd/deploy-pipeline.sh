17cbbd2c6d7199f229e37ba72db78c3d5c909681
Darin Webb <darinlwebb@gmail.com>
2022-03-08 15:58:07 -0600

    Set up codebuild for pull requests

---
+++ cicd/2-cicd/deploy-pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo Deploying Javabuilder CICD Pipeline
+
+# Create/Update the Javabuilder setup/dependencies stack. This is manually created and maintained, and requires elevated permissions. 
+
+# 'Developer' role requires a specific service role for all CloudFormation operations.
+if [[ $(aws sts get-caller-identity --query Arn --output text) =~ "475661607190:assumed-role/Developer/" ]]; then
+  # Append the role-arn option to the positional parameters $@ passed to cloudformation deploy.
+  set -- "$@" --role-arn "arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/admin/CloudFormationService"
+fi
+
+TEMPLATE_FILE=cicd/2-cicd/pipeline.template.yml
+
+echo Validating cloudformation template...
+aws cloudformation validate-template \
+  --template-body file://${TEMPLATE_FILE} \
+  | cat
+
+echo Updating cloudformation stack...
+aws cloudformation deploy \
+  --stack-name darin-javabuilder-ci \
+  --template-file ${TEMPLATE_FILE} \
+  --capabilities CAPABILITY_IAM \
+  "$@"
+
+echo Complete!
