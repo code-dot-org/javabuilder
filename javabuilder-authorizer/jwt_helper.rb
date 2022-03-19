a03987a55119a333a8f56a68a6db27edf8f5f4aa
Jessica Kulwik <jessica@code.org>
2022-03-18 17:39:30 -0700

    Cleanup duplicate code in the lambda authorizers

--- javabuilder-authorizer/jwt_helper.rb
+++ javabuilder-authorizer/jwt_helper.rb
@@ -1,53 +1,35 @@
 module JwtHelper
-  def generate_allow(principal_id, resource, token_payload)
+  # Verify the token with the appropriate public key (dependant on the
+  # environment the request came from), and checks the token has not
+  # expired and its issue time is not in the future.
+  def decode_token(token, origin)
+    return false unless token
+    begin
+      return JWT.decode(
+        token,
+        # Temporarily choose the key based on the client origin rather than the
+        # resource until we have environment-specific Javabuilders set up.
+        get_public_key(origin),
+        true,
+        verify_iat: true, # verify issued at time is valid
+        algorithm: 'RS256'
+      )
+    rescue JWT::ExpiredSignature, JWT::InvalidIatError
+      return false
+    end
+  end
+
+  def generate_allow(resource, decoded_token)
+    token_payload = decoded_token[0]
+    user_id = token_payload['uid']
+    issuer = token_payload['iss']
+    principal_id = "#{issuer}/#{user_id}"
     generate_policy(principal_id, 'Allow', resource, token_payload)
   end
 
-  def generate_deny(principal_id, resource)
-    generate_policy(principal_id, 'Deny', resource, nil)
+  def generate_deny(resource)
+    generate_policy(nil, 'Deny', resource, nil)
   end
-
-# Load the JWT public key from the appropriate environment variable
-# based on the environment the request was sent from.
-#
-# route_arn is in format
-# arn:aws:execute-api:region:account-id:api-id/stage-name/$connect
-# We only care about stage--that tells us the environment (development, staging, etc.)
-# def get_public_key(route_arn)
-def get_public_key(origin)
-  # Temporarily choose the key based on the client origin rather than the
-  # route_arn until we have environment-specific Javabuilders set up.
-  # tmp = route_arn.split(':')
-  # api_gateway_arn = tmp[5].split('/')
-  # stage_name = api_gateway_arn[1]
-
-  stage_name = ""
-  if origin.include? "localhost"
-    stage_name = "development"
-  elsif origin.include? "staging"
-    stage_name = "staging"
-  elsif origin.include? "levelbuilder"
-    stage_name = "levelbuilder"
-  elsif origin.include? "test"
-    stage_name = "test"
-  elsif origin.include? "adhoc"
-    stage_name = "adhoc"
-  else
-    stage_name = "production"
-  end
-  # End of temporary code
-
-  public_key = ENV["rsa_pub_#{stage_name}"]
-  # Environment variables can't contain newlines (if you copy over an environment variable with
-  # newlines they are replaced with spaces) The public keys are saved in the configuration by
-  # copying over the key with literal '\n' at the end of each line. Environment variable save
-  # then escapes '\n' and adds a space. We need to replace '\\n<space>' with a real '\n' for
-  # the key generation to work.
-  public_key = public_key.gsub('\n ', "\n")
-  OpenSSL::PKey::RSA.new(public_key)
-end
-
-  private
 
   # Helper function to generate an IAM policy.
   def generate_policy(principal_id, effect, resource, token_payload)
@@ -67,5 +49,52 @@ end
     end
     auth_response['context'] = token_payload if token_payload
     auth_response
+  end
+
+  private
+
+  # Load the JWT public key from the appropriate environment variable
+  # based on the environment the request was sent from.
+  #
+  # route_arn is in format
+  # arn:aws:execute-api:region:account-id:api-id/stage-name/$connect
+  # We only care about stage--that tells us the environment (development, staging, etc.)
+  # def get_public_key(route_arn)
+  def get_public_key(origin)
+    # Temporarily choose the key based on the client origin rather than the
+    # route_arn until we have environment-specific Javabuilders set up.
+    # tmp = route_arn.split(':')
+    # api_gateway_arn = tmp[5].split('/')
+    # stage_name = api_gateway_arn[1]
+
+    # TODO: Update the following strings to use the exact origin that requests come from.
+    # For now, log where the request came from so we can ensure we use the correct strings
+    # when we update this.
+    puts origin
+
+    stage_name = ""
+    if origin.include? "localhost"
+      stage_name = "development"
+    elsif origin.include? "staging"
+      stage_name = "staging"
+    elsif origin.include? "levelbuilder"
+      stage_name = "levelbuilder"
+    elsif origin.include? "test"
+      stage_name = "test"
+    elsif origin.include? "adhoc"
+      stage_name = "adhoc"
+    else
+      stage_name = "production"
+    end
+    # End of temporary code
+
+    public_key = ENV["rsa_pub_#{stage_name}"]
+    # Environment variables can't contain newlines (if you copy over an environment variable with
+    # newlines they are replaced with spaces) The public keys are saved in the configuration by
+    # copying over the key with literal '\n' at the end of each line. Environment variable save
+    # then escapes '\n' and adds a space. We need to replace '\\n<space>' with a real '\n' for
+    # the key generation to work.
+    public_key = public_key.gsub('\n ', "\n")
+    OpenSSL::PKey::RSA.new(public_key)
   end
 end
