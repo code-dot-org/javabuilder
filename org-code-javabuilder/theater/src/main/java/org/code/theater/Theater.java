7ec9652904afd87e7b5957129564c75861ee3aef
Sanchit Malhotra <sanchit@code.org>
2022-03-22 11:35:52 -0400

    Update Theater play methods

--- org-code-javabuilder/theater/src/main/java/org/code/theater/Theater.java
+++ org-code-javabuilder/theater/src/main/java/org/code/theater/Theater.java
@@ -9,12 +9,20 @@ public final class Theater {
   public static final Stage stage = new Stage();
   public static final Prompter prompter = new Prompter();
 
-  public static void play(Scene... scenes) {
+  public static void playScene(Scene scene) {
+    Theater.playScenes(scene);
+  }
+
+  public static void playScenes(Scene... scenes) {
     final List<SceneAction> allActions = new ArrayList<>();
     for (Scene scene : scenes) {
       allActions.addAll(scene.getActions());
     }
 
     TheaterPlayer.getInstance().play(allActions);
+  }
+
+  public static void play(Scene[] scenes) {
+    Theater.playScenes(scenes);
   }
 }
