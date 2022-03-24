3867e7f773a1caacc104d4e32b8ae7a03b36cb18
Sanchit Malhotra <sanchit@code.org>
2022-03-24 15:16:21 -0400

    add unit test and remove prints

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/AssetFileStubber.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/AssetFileStubber.java
@@ -4,6 +4,11 @@ import static org.code.protocol.LoggerNames.MAIN_LOGGER;
 
 import java.util.logging.Logger;
 
+/**
+ * Generates URLs for local, static asset files that can be used to stub asset file requests when
+ * URLs cannot be accessed, notably in the cases where asset URLs point to Dashboard running on
+ * localhost, or during an integration test.
+ */
 public class AssetFileStubber {
   private static final String[] IMAGE_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif"};
   private static final String[] AUDIO_EXTENSIONS = {".wav"};
@@ -16,23 +21,23 @@ public class AssetFileStubber {
   public AssetFileStubber() {
     stubImageUrl = getClass().getClassLoader().getResource(STUB_IMAGE_FILE_NAME).toString();
     stubAudioUrl = getClass().getClassLoader().getResource(STUB_AUDIO_FILE_NAME).toString();
-    System.out.printf(
-        "Generated stubbed URLs for image: %s and audio: %s\n", stubImageUrl, stubAudioUrl);
+  }
+
+  // Visible for testing
+  AssetFileStubber(String stubImageUrl, String stubAudioUrl) {
+    this.stubImageUrl = stubImageUrl;
+    this.stubAudioUrl = stubAudioUrl;
   }
 
   public String getStubAssetUrl(String filename) {
-    System.out.println("Stubbing asset file: " + filename);
     if (this.matchesExtension(filename, IMAGE_EXTENSIONS)) {
-      System.out.printf("Returning stubbed URL %s\n", stubImageUrl);
       return stubImageUrl;
     }
 
     if (this.matchesExtension(filename, AUDIO_EXTENSIONS)) {
-      System.out.printf("Returning stubbed URL %s\n", stubAudioUrl);
       return stubAudioUrl;
     }
 
-    System.out.println("Unknown extension; returning null");
     Logger.getLogger(MAIN_LOGGER)
         .warning(String.format("Unknown file %s. Cannot provide stubbed asset URL.", filename));
     return null;
@@ -44,7 +49,6 @@ public class AssetFileStubber {
         return true;
       }
     }
-
     return false;
   }
 }
