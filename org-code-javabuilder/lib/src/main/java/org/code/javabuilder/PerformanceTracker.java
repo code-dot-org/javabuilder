fdb0a8ee70cf6b5fd577e7c02d7ea8d203b46a84
Jessica Kulwik <jessica@code.org>
2022-03-25 17:32:57 -0700

    Added more detail to performance report

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/PerformanceTracker.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/PerformanceTracker.java
@@ -5,9 +5,11 @@ import static org.code.protocol.LoggerNames.MAIN_LOGGER;
 import java.time.Clock;
 import java.time.Instant;
 import java.util.logging.Logger;
+import org.code.protocol.LoggerConstants;
 import org.json.JSONObject;
 
 public class PerformanceTracker {
+  private static final String FIRST_INSTANCE = "firstInstance";
   private static final String COLD_BOOT_START = "coldBootStart";
   private static final String COLD_BOOT_END = "coldBootEnd";
   private static final String INSTANCE_START = "instanceStart";
@@ -16,16 +18,19 @@ public class PerformanceTracker {
   private static final String USER_CODE_START = "userCodeStart";
   private static final String USER_CODE_END = "userCodeEnd";
   private static final String INSTANCE_END = "instanceEnd";
+  private static final String TYPE = "performanceReport";
   private final JSONObject logs;
 
   public PerformanceTracker() {
     logs = new JSONObject();
   }
 
-  public void trackStartup(Instant coldBootStart, Instant coldBootEnd, Instant instanceStart) {
+  public void trackStartup(
+      Instant coldBootStart, Instant coldBootEnd, Instant instanceStart, boolean firstInstance) {
     logs.put(COLD_BOOT_START, coldBootStart.toString());
     logs.put(COLD_BOOT_END, coldBootEnd.toString());
     logs.put(INSTANCE_START, instanceStart.toString());
+    logs.put(FIRST_INSTANCE, firstInstance);
   }
 
   public void trackCompileStart() {
@@ -49,6 +54,7 @@ public class PerformanceTracker {
   }
 
   public void logPerformance() {
+    logs.put(LoggerConstants.TYPE, TYPE);
     Logger.getLogger(MAIN_LOGGER).info(logs.toString());
   }
 }
