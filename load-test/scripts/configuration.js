6188720af6e2b91e560a4752ce5dfb3599338ee4
molly-moen <molly@code.org>
2022-04-01 16:50:03 -0700

    works locally

--- load-test/scripts/configuration.js
+++ load-test/scripts/configuration.js
@@ -1,21 +1,21 @@
 export const basicTestOptions = {
   scenarios: {
     // ramp up to 5 VUs over 30 seconds
-    rampUp: {
-      executor: "ramping-vus",
-      startVUs: 0,
-      stages: [
-        {duration: '30s', target: 5}
-      ]
-    },
+    // rampUp: {
+    //   executor: "ramping-vus",
+    //   startVUs: 0,
+    //   stages: [
+    //     {duration: '30s', target: 5}
+    //   ]
+    // },
     // have 5 VUs do 3 iterations each, up to a max of 2 minutes. Start this after
     // the ramp up time.
     highLoad: {
       executor: "per-vu-iterations",
-      vus: 5,
-      iterations: 3,
+      vus: 1,
+      iterations: 1,
       maxDuration: "2m",
-      startTime: '30s',
+      //startTime: '30s',
     }
   },
   thresholds: {
@@ -31,8 +31,7 @@ export const basicTestOptions = {
 // TODO: Update to a load testing instance of Javabuilder
 export const uploadUrl = `https://javabuilder-molly-http.dev-code.org/seedsources/sources.json?Authorization=`;
 export const url = `wss://javabuilder-molly.dev-code.org?Authorization=`;
-// TODO: update to 'load-test' once we have load test auth set up
-const origin = "http://localhost-studio.code.org:3000";
+const origin = "load-test";
   
 export const websocketParams = {
   headers: {
@@ -48,8 +47,7 @@ export const uploadParams = {
 };
 
 // These will be used for generating the JWT token
-export const PRIVATE_KEY = __ENV.PRIVATE_KEY;
-export const PRIVATE_KEY_PASSWORD = __ENV.PRIVATE_KEY_PASSWORD;
+export const PRIVATE_KEY = null;
 
 // Thresholds for metrics
 export const LONG_REQUEST_MS = 5000;
