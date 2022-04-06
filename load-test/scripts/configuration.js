b815be4ff80e1bcaf65dfc2c946e545e325aecbc
molly-moen <molly@code.org>
2022-04-06 11:14:47 -0700

    updates to script

--- load-test/scripts/configuration.js
+++ load-test/scripts/configuration.js
@@ -34,6 +34,9 @@ export const MiniAppType = {
 
 export function getTestOptions(maxUserGoal, rampUpTimeMinutes, highLoadTimeMinutes) {
   const maxConcurrentUsers =  Math.floor(maxUserGoal / 30);
+  // set constant VUs to be slightly higher than max concurrent users to account for users taking slightly different
+  // amounts of time
+  const constantVUs =  Math.floor(maxUserGoal / 25);
   return  {
     scenarios: {
       // ramp up to maxConcurrentUsers VUs over rampUpTimeMinutes
@@ -44,15 +47,16 @@ export function getTestOptions(maxUserGoal, rampUpTimeMinutes, highLoadTimeMinut
           {duration: `${rampUpTimeMinutes}m`, target: maxConcurrentUsers}
         ]
       },
-      // have maxConcurrentUsers VUs do 3 iterations each minute, for a total of highLoadTimeMinutes * 3
+      // have constantVUs VUs do 3 iterations each minute, for a total of highLoadTimeMinutes * 3
       // iterations per virutal user.
       // Start this after the ramp up time and allow for 1 extra minute in the max duration in case of issues.
       highLoad: {
         executor: "per-vu-iterations",
-        vus: maxConcurrentUsers,
+        vus: constantVUs,
         iterations: 3 * highLoadTimeMinutes, // this is iterations per virtual user
-        maxDuration: `${highLoadTimeMinutes + 1}m`,
-        startTime: `${rampUpTimeMinutes + 1}m`,
+        // add a large buffer to max duration to ensure all requests can complete.
+        maxDuration: `${highLoadTimeMinutes + (Math.floor(highLoadTimeMinutes * 0.5))}m`,
+        startTime: `${rampUpTimeMinutes}m`,
       }
     },
     thresholds: {
