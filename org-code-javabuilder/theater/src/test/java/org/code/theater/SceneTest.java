864fa51c2a660476f6c10c645a83aa0c700a1324
Ben Brooks <ben@code.org>
2022-04-11 15:52:17 -0700

    Copy image before draw

--- org-code-javabuilder/theater/src/test/java/org/code/theater/SceneTest.java
+++ org-code-javabuilder/theater/src/test/java/org/code/theater/SceneTest.java
@@ -3,7 +3,6 @@ package org.code.theater;
 import static org.code.theater.Scene.*;
 import static org.code.theater.support.DrawImageAction.UNSPECIFIED;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 import org.code.media.Color;
 import org.code.media.Font;
@@ -65,7 +64,7 @@ class SceneTest {
 
   @Test
   public void testDrawImage() {
-    final Image image = mock(Image.class);
+    final Image image = new Image(1, 1);
     final int x = 100;
     final int y = 100;
     final int size = 200;
@@ -74,7 +73,10 @@ class SceneTest {
     final double rotation = 90.0;
 
     unitUnderTest.drawImage(image, x, y, size);
-    assertSame(image, getLastAction(DrawImageAction.class).getImage());
+    // Confirm we copy the image object when drawing it
+    assertNotSame(image, getLastAction(DrawImageAction.class).getImage());
+    assertEquals(1, getLastAction(DrawImageAction.class).getImage().getHeight());
+
     assertEquals(x, getLastAction(DrawImageAction.class).getX());
     assertEquals(y, getLastAction(DrawImageAction.class).getY());
     assertEquals(size, getLastAction(DrawImageAction.class).getSize());
