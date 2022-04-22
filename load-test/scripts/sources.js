38b8e26d81754fc34dda93c5337043625d42ec41
Ben Brooks <ben@code.org>
2022-04-22 15:40:24 -0700

    Scanner sources

--- load-test/scripts/sources.js
+++ load-test/scripts/sources.js
@@ -23,3 +23,11 @@ export const neighborhood = JSON.stringify({
   },
   "assetUrls":{}
 });
+
+export const scanner = JSON.stringify({
+  sources: {
+    "main.json":
+      '{"source":{"MyScanner.java":{"text":"import java.util.Scanner;\\n\\npublic class MyScanner {\\n  public static void main(String[] args) {\\n    Scanner myScanner = new Scanner(System.in);\\n    System.out.println(\\"What\'s your name?\\");\\n    String response = myScanner.nextLine();\\n    System.out.println(\\"Hello \\" + response + \\"!\\");\\n  }\\n}\\n","isVisible":true}},"animations":{}}'
+  }
+});
+
