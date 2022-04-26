ddd70da7bbf14d32c8654b548e5dbabae7229ecd
Darin Webb <darinlwebb@gmail.com>
2022-04-26 10:43:32 -0700

    generate cloudformation config files

---
+++ cicd/3-app/javabuilder/config/create-environment-config.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+dir=cicd/3-app/javabuilder
+
+# loop over config files
+for i in $(ls "${dir}/config" | egrep -i '.*\.config\.json' ); do
+  file="${dir}/config/${i}"
+  echo "tranforming ${i}..."
+  contents=$(cat $file)
+
+  # New value insertion
+  contents="$(jq '.NewValueDemo = "abcde"' <<< $contents)"
+
+  # Edit existing value
+  if [ "$( jq 'has("TransformDemo")' $file )" == "true" ]; then
+    ORIGINAL_VALUE=$(jq -r  '.TransformDemo' $file)
+    NEW_VALUE=$(date '+%s')
+    echo "replacing '${ORIGINAL_VALUE}' with '${NEW_VALUE}'"
+    contents="$(jq ".TransformDemo = ${NEW_VALUE}" <<< $contents)"
+    # jq ".TransformDemo = ${NEW_VALUE}" $file > $file
+  fi
+
+  echo "${contents}" > "${dir}/${i}"
+done
+
+
