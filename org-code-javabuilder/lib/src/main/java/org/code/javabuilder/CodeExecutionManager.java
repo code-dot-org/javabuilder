45ffbf0ce1026173b6e6143b1d66d319c43e3b53
Sanchit Malhotra <sanchit@code.org>
2022-04-26 16:54:17 -0700

    remove print statement

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/CodeExecutionManager.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/CodeExecutionManager.java
@@ -150,7 +150,6 @@ public class CodeExecutionManager {
     // Create temp folder
     try {
       this.tempFolder = Files.createTempDirectory("tmpdir").toFile();
-      System.out.println("Execution temp folder: " + this.tempFolder.toPath());
     } catch (IOException e) {
       throw new InternalServerException(InternalExceptionKey.INTERNAL_EXCEPTION, e);
     }
