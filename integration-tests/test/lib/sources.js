cc382d4bec81369d1c50e38da6158ad407f55924
Sanchit Malhotra <sanchit@code.org>
2022-05-05 12:36:27 -0700

    Add and run basic integration tests

---
+++ integration-tests/test/lib/sources.js
@@ -0,0 +1,7 @@
+export const helloWorld = JSON.stringify({
+  sources: {
+    "main.json":
+      '{"source":{"HelloWorld.java":{"text":"public class HelloWorld {\\n  public static void main(String[] args)  {\\n    System.out.println(\\"Hello World\\");\\n  }\\n}","isVisible":true}},"animations":{}}',
+  },
+  assetUrls: {},
+});
