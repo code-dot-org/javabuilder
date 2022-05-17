7960b754ed84449963fb22a55b11e0824616027b
Ben Brooks <ben@code.org>
2022-05-17 16:24:54 -0700

    Working theater test, refactor all tests to use helper

--- integration-tests/test/helloWorld.js
+++ integration-tests/test/helloWorld.js
@@ -1,17 +1,19 @@
 import { helloWorld } from "./lib/sources.js";
 import { NEIGHBORHOOD } from "./lib/MiniAppType.js";
-import {assertMessagesEqual, verifyMessagesReceived} from "./helpers/testHelpers.js";
-import { expect } from "chai";
+import {
+  assertMessagesEqual,
+  verifyMessagesReceived,
+  INITIAL_STATUS_MESSAGES,
+  EXIT_STATUS_MESSAGE,
+} from "./helpers/testHelpers.js";
 
 describe("Hello World", () => {
   it("Runs Hello World project", (done) => {
     const expectedMessages = [
-      { type: "STATUS", value: "COMPILING" },
-      { type: "STATUS", value: "COMPILATION_SUCCESSFUL" },
-      { type: "STATUS", value: "RUNNING" },
+      ...INITIAL_STATUS_MESSAGES,
       { type: "SYSTEM_OUT", value: "Hello World" },
       { type: "SYSTEM_OUT", value: "\n" },
-      { type: "STATUS", value: "EXITED" },
+      EXIT_STATUS_MESSAGE
     ];
     const assertOnMessagesReceived = receivedMessages => assertMessagesEqual(receivedMessages, expectedMessages);
 
