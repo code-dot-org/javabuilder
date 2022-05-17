199b49a7d2fc63435b78ccbd599a2c9458db92e1
molly-moen <molly@code.org>
2022-05-17 10:07:18 -0700

    clean up

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/AWSMetricClient.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/AWSMetricClient.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.cloudwatch.model.*;
 import org.code.protocol.MetricClient;
 
 // Metric Client which published metrics to AWS CloudWatch.
-// Requires cloudwatch:PutMetricData Permission on the Lambda.
+// Requires cloudwatch:PutMetricData permission on the Lambda.
 public class AWSMetricClient implements MetricClient {
   private final AmazonCloudWatch cloudWatchClient;
   private final Dimension functionNameDimension;
@@ -15,20 +15,13 @@ public class AWSMetricClient implements MetricClient {
 
   public AWSMetricClient(String functionName) {
     this.cloudWatchClient = AmazonCloudWatchClientBuilder.defaultClient();
+    // this will split out metrics by function name in CloudWatch
     this.functionNameDimension = new Dimension().withName("functionName").withValue(functionName);
   }
 
   @Override
   public void publishSevereError() {
-    MetricDatum metricDatum =
-        new MetricDatum()
-            .withMetricName("SevereError")
-            .withUnit(StandardUnit.Count)
-            .withValue(1.0)
-            .withDimensions(this.functionNameDimension);
-    PutMetricDataRequest request =
-        new PutMetricDataRequest().withNamespace(NAMESPACE).withMetricData(metricDatum);
-    this.cloudWatchClient.putMetricData(request);
+    this.publishCountMetric("SevereError", 1.0);
   }
 
   @Override
@@ -57,6 +50,18 @@ public class AWSMetricClient implements MetricClient {
             .withMetricName(metricName)
             .withUnit(StandardUnit.Milliseconds)
             .withValue(milliseconds)
+            .withDimensions(this.functionNameDimension);
+    PutMetricDataRequest request =
+        new PutMetricDataRequest().withNamespace(NAMESPACE).withMetricData(metricDatum);
+    this.cloudWatchClient.putMetricData(request);
+  }
+
+  private void publishCountMetric(String metricName, double count) {
+    MetricDatum metricDatum =
+        new MetricDatum()
+            .withMetricName(metricName)
+            .withUnit(StandardUnit.Count)
+            .withValue(count)
             .withDimensions(this.functionNameDimension);
     PutMetricDataRequest request =
         new PutMetricDataRequest().withNamespace(NAMESPACE).withMetricData(metricDatum);
