8d83074331ca0b8dbfef41e9fcad6150701f4d3d
Ben Brooks <ben@code.org>
2022-05-18 14:17:51 -0700

    Use base domain and sub domain in theater test

--- integration-tests/test/lib/environment.js
+++ integration-tests/test/lib/environment.js
@@ -12,3 +12,6 @@ export const PASSWORD = getSecretString(process.env.JAVABUILDER_PASSWORD);
 
 export const JAVABUILDER_HTTP_URL = process.env.JAVABUILDER_HTTP_URL;
 export const JAVABUILDER_WEBSOCKET_URL = process.env.JAVABUILDER_WEBSOCKET_URL;
+
+export const JAVABUILDER_BASE_DOMAIN = process.env.JAVABUILDER_BASE_DOMAIN;
+export const JAVABUILDER_SUB_DOMAIN = process.env.JAVABUILDER_SUB_DOMAIN;
