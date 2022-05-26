b48ed3e93d179721a15bada2ba230f41d1138447
Ben Brooks <ben@code.org>
2022-05-26 10:00:32 -0700

    Move websocket metrics into helper

---
+++ javabuilder-authorizer/metrics_reporter.rb
@@ -0,0 +1,38 @@
+require 'aws-sdk-cloudwatch'
+
+class MetricsReporter
+  include TokenStatus
+
+  def initialize(context)
+    @context = context
+    @client = Aws::CloudWatch::Client.new(region: region)
+  end
+
+  def log_error(status, error_message)
+    puts error_message
+    metric_data = {
+      metric_name: TOKEN_STATUS_METRIC_NAMES[status],
+      dimensions: [
+        {
+          name: "functionName",
+          value: function_name
+        }
+      ],
+      unit: "Count",
+      value: 1
+    }
+
+    @client.put_metric_data({namespace: "Javabuilder", metric_data: [metric_data]})
+  end
+
+  private
+
+  # ARN is of the format arn:aws:lambda:{region}:{account_id}:function:{lambda_name}
+  def region
+    @context.invoked_function_arn.split(':')[3]
+  end
+
+  def function_name
+    @context.function_name
+  end
+end
