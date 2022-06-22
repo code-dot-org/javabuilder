0de7e7409d476bd9c21bd1fc8d68cb98e9eed2d2
molly-moen <molly@code.org>
2022-06-22 11:41:41 -0700

    add class check and tests

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaRequestHandler.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaRequestHandler.java
@@ -99,11 +99,7 @@ public class LambdaRequestHandler implements RequestHandler<Map<String, String>,
   public String handleRequest(Map<String, String> lambdaInput, Context context) {
     this.isSessionInitialized = false;
     this.trackStartupPerformance();
-    if (JavabuilderContext.getInstance() == null) {
-      JavabuilderContext.create();
-    } else {
-      JavabuilderContext.getInstance().destroyAndReset();
-    }
+    JavabuilderContext.getInstance().destroyAndReset();
 
     // TODO: Because we reference the logger object throughout the codebase via
     // Logger.getLogger(MAIN_LOGGER), we need to set it up in the same scope as code execution to
