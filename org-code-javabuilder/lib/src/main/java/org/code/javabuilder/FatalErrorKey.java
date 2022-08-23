91cb70b9ab07d3ccbcae2af88813e92228b8721f
Sanchit Malhotra <sanchit@code.org>
2022-08-23 15:36:45 -0500

    Force recycle container if connection pool has been shut down

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/FatalErrorKey.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/FatalErrorKey.java
@@ -1,11 +1,11 @@
 package org.code.javabuilder;
 
-import static org.code.javabuilder.LambdaErrorCodes.LOW_DISK_SPACE_ERROR_CODE;
-import static org.code.javabuilder.LambdaErrorCodes.TEMP_DIRECTORY_CLEANUP_ERROR_CODE;
+import static org.code.javabuilder.LambdaErrorCodes.*;
 
 public enum FatalErrorKey {
   LOW_DISK_SPACE(LOW_DISK_SPACE_ERROR_CODE),
-  TEMP_DIRECTORY_CLEANUP_ERROR(TEMP_DIRECTORY_CLEANUP_ERROR_CODE);
+  TEMP_DIRECTORY_CLEANUP_ERROR(TEMP_DIRECTORY_CLEANUP_ERROR_CODE),
+  CONNECTION_POOL_SHUT_DOWN(CONNECTION_POOL_SHUT_DOWN_ERROR_CODE);
 
   private final int errorCode;
 
--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaErrorCodes.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaErrorCodes.java
@@ -9,4 +9,5 @@ public final class LambdaErrorCodes {
   public static final int LOW_DISK_SPACE_ERROR_CODE = 50;
   public static final int OUT_OF_MEMORY_ERROR_CODE = 60;
   public static final int UNHEALTHY_CONTAINER_ERROR_CODE = 70;
+  public static final int CONNECTION_POOL_SHUT_DOWN_ERROR_CODE = 70;
 }
