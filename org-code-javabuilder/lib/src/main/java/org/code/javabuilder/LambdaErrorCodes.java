9c73332f0566ab84890fe59300f67954563e0227
Sanchit Malhotra <sanchit@code.org>
2022-08-24 15:56:55 -0500

    update error codes

--- org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaErrorCodes.java
+++ org-code-javabuilder/lib/src/main/java/org/code/javabuilder/LambdaErrorCodes.java
@@ -8,6 +8,6 @@ public final class LambdaErrorCodes {
   public static final int TEMP_DIRECTORY_CLEANUP_ERROR_CODE = 10;
   public static final int LOW_DISK_SPACE_ERROR_CODE = 50;
   public static final int OUT_OF_MEMORY_ERROR_CODE = 60;
-  public static final int UNHEALTHY_CONTAINER_ERROR_CODE = 70;
   public static final int CONNECTION_POOL_SHUT_DOWN_ERROR_CODE = 70;
+  public static final int UNHEALTHY_CONTAINER_ERROR_CODE = 1; // Test
 }
