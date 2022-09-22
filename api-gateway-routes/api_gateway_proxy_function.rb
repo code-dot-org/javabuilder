c1125ad02073baf8dc212b3dcd3ad22e291a336b
molly-moen <molly@code.org>
2022-09-22 10:55:29 -0700

    update logs

--- api-gateway-routes/api_gateway_proxy_function.rb
+++ api-gateway-routes/api_gateway_proxy_function.rb
@@ -37,12 +37,12 @@ def on_connect(event, context)
   # connection is made.
   if authorization_error_response
     # Add some logging to make sure we know this is happening.
-    p get_formatted_log("AUTHORIZATION ERROR: #{authorization_error_response[:body]}", request_context)
+    puts get_formatted_log("AUTHORIZATION ERROR: #{authorization_error_response[:body]}", request_context, nil)
     return { statusCode: 200, body: authorization_error_response[:body] }
   end
 
   # log entire request context object for debugging
-  p get_formatted_log("CONNECT REQUEST CONTEXT: #{request_context}", request_context)
+  puts get_formatted_log("CONNECT REQUEST CONTEXT: #{request_context}", request_context, nil)
 
   # Return early if this is the user connectivity test
   if is_connectivity_test(authorizer)
@@ -82,7 +82,7 @@ def on_connect(event, context)
   else
     # log so we know we saw an invalid mini app
     invalid_message = "invalid mini-app"
-    p get_formatted_log(invalid_message, request_context, 400)
+    puts get_formatted_log(invalid_message, request_context, 400)
     return { statusCode: 400, body: invalid_message }
   end
 
@@ -204,7 +204,7 @@ def sqs_operation_with_retries(sqs_operation, request_context, *operation_params
     raise(e) if retries >= MAX_SQS_RETRIES
     retries += 1
     sleep(sleep_time)
-    puts "RETRY #{retries}, just slept for #{sleep_time} seconds. Request context: #{request_context}"
+    puts get_formatted_log("RETRY #{retries}, just slept for #{sleep_time} seconds", request_context, nil)
     # double sleep time for next retry
     sleep_time = sleep_time * 2
     retry
@@ -232,16 +232,16 @@ def delete_queue(sqs_client, event, context)
     # This exception is expected during connectivity tests,
     # so do not log in those cases.
     unless is_connectivity_test(authorizer)
-      puts "DISCONNECT ERROR REQUEST CONTEXT: #{request_context}"
-      puts "DISCONNECT ERROR: #{e.message}"
+      puts get_formatted_log("DISCONNECT ERROR: #{e.message}", request_context, nil)
     end
   end
+end
 
-  def get_formatted_log(log_message, request_context, status_code)
-    log_data = {
-      connectionId: request_context['connectionId'],
-      message: log_message
-    }
-    log_data[:status_code] = status_code if status_code
-    log_data
+def get_formatted_log(log_message, request_context, status_code)
+  log_data = {
+    connectionId: request_context['connectionId'],
+    message: log_message
+  }
+  log_data[:status_code] = status_code if status_code
+  log_data.to_json
 end
